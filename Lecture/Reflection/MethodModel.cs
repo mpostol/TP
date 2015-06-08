37aaded8a201b711bc0a2089e71c60fbd661c1e2
ignas456 <ignaczewski456@hotmail.com>
2015-06-08 17:47:03 +0200

    Reflection

---
+++ Lecture/Reflection/MethodModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionProject.Model
+{
+    public class MethodModel
+    {
+        [System.Xml.Serialization.XmlAttribute("name")]
+        public string Name { get; set; }
+        
+        [System.Xml.Serialization.XmlElement("returnParameter")]
+        public ClassModel ReturnType { get; set; }
+
+        [System.Xml.Serialization.XmlElement("parameter")]
+        public ObservableCollection<ClassModel> ParameterList { get; set; }
+        
+        public MethodModel()
+        {
+            ParameterList = new ObservableCollection<ClassModel>();
+        }
+    }
+}
---
+++ Lecture/Reflection/NamespaceModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionProject.Model
+{
+    public class NamespaceModel
+    {
+        [System.Xml.Serialization.XmlAttribute("name")]
+        public string Name { get; set; }
+        
+        [System.Xml.Serialization.XmlElement("class")]
+        public ObservableCollection<ClassModel> Classes { get; set; }
+
+        public NamespaceModel()
+        {
+            Classes = new ObservableCollection<ClassModel>();
+        }
+    }
+}
