7681e77feb7d855cc8a520cb7e421c5da79198e9
ignas456 <ignaczewski456@hotmail.com>
2015-06-22 13:49:49 +0200

    Update of the model from Reflection Project. Analysing assembly is divided iinto smaller parts.

--- Lecture/Reflection/AssemblyModel.cs
+++ Lecture/Reflection/AssemblyModel.cs
@@ -12,8 +12,10 @@ namespace ReflectionProject.Model
     public class AssemblyModel
     {
         [System.Xml.Serialization.XmlAttribute("name")]
+        //Name of the assembly.
         public string Name { get; set; }
         [System.Xml.Serialization.XmlElement("namespace")]
+        //Collection of namespaces in assembly.
         public ObservableCollection<NamespaceModel> Namespaces { get; set; }
         public AssemblyModel()
         {
@@ -21,101 +23,35 @@ namespace ReflectionProject.Model
             _classDictionary = new Dictionary<string, ClassModel>();
         }
 
-        private Dictionary<string, ClassModel> _classDictionary;
+        public Dictionary<string, ClassModel> _classDictionary;
         private Assembly _assembly;
 
-        public void AnalyzeAssembly(string path)
+        public void AnalyseAssembly(string path)
         {
             _assembly = Assembly.LoadFile(path);
             this.Name = _assembly.GetName().ToString();
             GetNamespaces();
-            foreach (NamespaceModel n in Namespaces)
-            {
-                IEnumerable<Type> classes = from c in _assembly.GetTypes()
-                                            where c.Namespace == n.Name && c.IsClass && GetVisible(c)
-                                            select c;
-                foreach (Type c in classes)
-                {
-                    if (!_classDictionary.ContainsKey(c.Name))
-                        _classDictionary.Add(c.Name, new ClassModel() { Name = c.Name, Namespace = c.Namespace });
-                    n.Classes.Add(_classDictionary[c.Name]);
-
-                    foreach (ClassModel cls in n.Classes)
-                    {
-                        if (!_classDictionary.ContainsKey(c.BaseType.Name))
-                            _classDictionary.Add(c.BaseType.Name, new ClassModel() { Name = c.BaseType.Name, Namespace = c.BaseType.Namespace });
-                        cls.BaseType = _classDictionary[c.BaseType.Name];
-
-
-                        IEnumerable<Type> interfaces = c.GetInterfaces();
-                        foreach (Type i in interfaces)
-                        {
-                            if (!_classDictionary.ContainsKey(i.Name))
-                                _classDictionary.Add(i.Name, new ClassModel()
-                                {
-                                    Name = i.Name,
-                                    Namespace = i.Namespace
-                                });
-                            cls.Interfaces.Add(_classDictionary[i.Name]);
-
-                        }
-
-                        IEnumerable<FieldInfo> fields = c.GetFields();
-                        foreach (FieldInfo f in fields)
-                        {
-                            if (!_classDictionary.ContainsKey(f.FieldType.Name))
-                                _classDictionary.Add(f.FieldType.Name, new ClassModel() { Name = f.FieldType.Name });
-                            cls.Fields.Add(_classDictionary[f.FieldType.Name]);
-                        }
-
-                        IEnumerable<PropertyInfo> props = c.GetProperties();
-                        foreach (PropertyInfo p in props)
-                        {
-                            if (!_classDictionary.ContainsKey(p.PropertyType.Name))
-                                _classDictionary.Add(p.PropertyType.Name, new ClassModel() { Name = p.PropertyType.Name });
-                            cls.Properties.Add(_classDictionary[p.PropertyType.Name]);
-                        }
-
-                        IEnumerable<MethodInfo> methods = c.GetMethods();
-                        foreach (MethodInfo m in methods)
-                        {
-                            cls.Methods.Add(new MethodModel() { Name = m.Name });
-
-                            foreach (MethodModel meth in cls.Methods)
-                            {
-                                if (!_classDictionary.ContainsKey(m.ReturnType.Name))
-                                    _classDictionary.Add(m.ReturnType.Name, new ClassModel() { Name = m.ReturnType.Name, Namespace = m.ReturnType.Namespace });
-                                meth.ReturnType = _classDictionary[m.ReturnType.Name];
-
-                                IEnumerable<ParameterInfo> pars = m.GetParameters();
-                                foreach (ParameterInfo p in pars)
-                                {
-                                    if (!_classDictionary.ContainsKey(p.ParameterType.Name))
-                                        _classDictionary.Add(p.ParameterType.Name, new ClassModel() { Name = p.ParameterType.Name, Namespace = p.ParameterType.Namespace });
-                                    meth.ParameterList.Add(_classDictionary[p.ParameterType.Name]);
-                                }
-                            }
-                        }
-                    }
-
-
-                }
-            }
-
         }
-        private bool GetVisible(Type t)
+        public static bool GetVisible(Type t)
         {
             return t.IsPublic || t.IsNestedPublic || t.IsNestedFamily || t.IsNestedFamANDAssem;
         }
-        private void GetNamespaces()
+        public void GetNamespaces()
         {
             IEnumerable<string> namespaces = from c in _assembly.GetTypes()
                                              where GetVisible(c)
                                              select c.Namespace;
             foreach (string n in namespaces.Distinct())
             {
-                Namespaces.Add(new NamespaceModel() { Name = n });
+                NamespaceModel nmspc = new NamespaceModel() { Name = n };
+                Namespaces.Add(nmspc);
+                nmspc.AnalyseNamespace(_assembly, _classDictionary);
             }
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
         }
     }
 }
--- Lecture/Reflection/ClassModel.cs
+++ Lecture/Reflection/ClassModel.cs
@@ -1,36 +1,54 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ReflectionProject.Model
 {
-    public class ClassModel
+    public class ClassModel : INotifyPropertyChanged
     {
         [System.Xml.Serialization.XmlAttribute("name")]
+        //Name of the class.
         public string Name { get; set; }
 
         [System.Xml.Serialization.XmlAttribute("namespace")]
+        //Name of the namespace where class is included.
         public string Namespace { get; set; }
 
         [System.Xml.Serialization.XmlElement("baseType")]
-        public ClassModel BaseType { get; set; }
+        //Base type of class.
+        public ClassModel BaseType
+        {
+            get { return _baseType; }
+            set
+            {
+                _baseType = value;
+                OnPropertyChanged("BaseType");
+            }
+        }
+        private ClassModel _baseType;
 
         [System.Xml.Serialization.XmlElement("interface")]
+        //Implemented interfaces in class.
         public ObservableCollection<ClassModel> Interfaces { get; set; }
 
         [System.Xml.Serialization.XmlElement("field")]
+        //Collection of fields.
         public ObservableCollection<ClassModel> Fields { get; set; }
 
         [System.Xml.Serialization.XmlElement("property")]
+        //Collection of properties.
         public ObservableCollection<Model.ClassModel> Properties { get; set; }
 
         [System.Xml.Serialization.XmlElement("method")]
+        //Collection of methods.
         public ObservableCollection<Model.MethodModel> Methods { get; set; }
 
-        
 
         public ClassModel()
         {
@@ -39,6 +57,84 @@ namespace ReflectionProject.Model
             Properties = new ObservableCollection<ClassModel>();
             Interfaces = new ObservableCollection<ClassModel>();
         }
-       
+
+        //Method analysing Class
+        public void AnalyseClass(Type type, Dictionary<string, ClassModel> classDictionary)
+        {
+            this.BaseType = GetBaseType(type, classDictionary);
+            GetInterfaces(type, classDictionary);
+            GetFields(type, classDictionary);
+            GetProperties(type, classDictionary);
+            GetMethods(type, classDictionary);
+        }
+
+        public ClassModel GetBaseType(Type type, Dictionary<string, ClassModel> classDictionary)
+        {
+            if (type.BaseType != null)
+            {
+                if (!classDictionary.ContainsKey(type.BaseType.Name))
+                    classDictionary.Add(type.BaseType.Name, new ClassModel() { Name = type.BaseType.Name, Namespace = type.BaseType.Namespace });
+                return classDictionary[type.BaseType.Name];
+            }
+            return null;
+        }
+
+        public void GetInterfaces(Type type, Dictionary<string, ClassModel> classDictionary)
+        {
+            IEnumerable<Type> interfaces = type.GetInterfaces();
+            foreach (Type i in interfaces)
+            {
+                if (!classDictionary.ContainsKey(i.Name))
+                    classDictionary.Add(i.Name, new ClassModel()
+                    {
+                        Name = i.Name,
+                        Namespace = i.Namespace
+                    });
+                this.Interfaces.Add(classDictionary[i.Name]);
+            }
+        }
+
+        public void GetFields(Type type, Dictionary<string, ClassModel> classDictionary)
+        {
+            IEnumerable<FieldInfo> fields = type.GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                if (!classDictionary.ContainsKey(field.FieldType.Name))
+                    classDictionary.Add(field.FieldType.Name, new ClassModel() { Name = field.FieldType.Name });
+                this.Fields.Add(classDictionary[field.FieldType.Name]);
+            }
+        }
+
+        public void GetProperties(Type type, Dictionary<string, ClassModel> classDictionary)
+        {
+            IEnumerable<PropertyInfo> properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                if (!classDictionary.ContainsKey(property.PropertyType.Name))
+                    classDictionary.Add(property.PropertyType.Name, new ClassModel() { Name = property.PropertyType.Name });
+                this.Properties.Add(classDictionary[property.PropertyType.Name]);
+            }
+        }
+
+        public void GetMethods(Type type, Dictionary<string, ClassModel> classDictionary)
+        {
+            IEnumerable<MethodInfo> methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                MethodModel temp = new MethodModel() { Name = method.Name };
+                this.Methods.Add(temp);
+                temp.AnalyseMethod(method, classDictionary);
+            }
+        }
+
+        //Must be implemented because of BaseType property.
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged = null;
+        virtual protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+        #endregion
     }
 }
