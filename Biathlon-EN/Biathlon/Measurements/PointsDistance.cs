6dea4df538be00a265fd7403b57552957f7e9449
Wiktor Wandachowicz <wiktor.wandachowicz@p.lodz.pl>
2016-03-26 16:21:36 +0100

    #13 - Biathlon - calculating the distance between two points

---
+++ Biathlon-EN/Biathlon/Measurements/PointsDistance.cs
@@ -0,0 +1,28 @@
+﻿using MetricsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biathlon.Measurements
+{
+    public class PointsDistance
+    {
+        private IDistanceMetric Gauge;
+
+        public PointsDistance(IDistanceMetric gauge)
+        {
+            if (gauge == null)
+                throw new ArgumentNullException(nameof(gauge));
+            Gauge = gauge;
+        }
+
+        public double CalculateDistance(Point p1, Point p2)
+        {
+            double[] a = { p1.X, p1.Y };
+            double[] b = { p2.X, p2.Y };
+            return Gauge.CalculateDistance(a, b);
+        }
+    }
+}
---
+++ Biathlon-EN/BiathlonTests/README.md
@@ -0,0 +1,9 @@
+﻿# BiathlonTests
+
+The collection of unit tests for project **Biathlon**.
+
+## PointsDistanceTests
+
+### Dependency Injection
+
+Each test composes `PointsDistance` instance by passing as its constructor's parameter one of `IDistanceMetric` implementations.
