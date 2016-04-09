39aa60f60a7404b347254e8f3dbe12c09cfeda1c
Mariusz <mpostol@cas.eu>
2016-04-09 23:47:25 +0200

    #16 - added and tested - SelfDictionary must be reviewed.

---
+++ Lecture/Lecture/Generics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP.Lecture
+{
+  public class Generics<ClassType>
+  {
+    public ClassType DefaultValue { get; private set; }
+    public Dictionary<int, ClassType> DictionaryPropert { get; private set; }
+    public Generics()
+    {
+      DefaultValue = default(ClassType);
+      DictionaryPropert = new Dictionary<int, ClassType>();
+    }
+    public ClassType GenericMethod<MethodType>(ClassType classTypeParameter)
+    {
+      return classTypeParameter;
+    }
+    public bool GenericMethod<MethodType>(ClassType parameter1, ClassType parameter2)
+    {
+      return parameter1.Equals(parameter2);
+    }
+    public MethodType GenericMethod<MethodType>(MethodType methodTypeParameter)
+    {
+      return methodTypeParameter;
+    }
+    public static ClassType StaticMethod (ClassType parameter)
+    {
+      return parameter;
+    }
+  }
+
+  public class SelfDictionary<Type>: Dictionary<Type, Type>
+    where Type: IEquatable<Type>  //https://msdn.microsoft.com/en-us/library/d5x73970.aspx
+  {
+    public void AddIfNotPresent(Type entity)
+    {
+      if (base.ContainsKey(entity))
+        return;
+      base.Add(entity, entity);
+    }
+  }
+}
---
+++ Lecture/Lesson2UnitTest/GenericsUnitTest.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TP.Lecture.UnitTest
+{
+  [TestClass]
+  public class GenericsUnitTest
+  {
+    [TestMethod]
+    public void AfterCreatioValueTestMethod()
+    {
+      Generics<int> _intInstance = new Generics<int>();
+      Assert.AreEqual<int>(0, _intInstance.DefaultValue);
+    }
+    [TestMethod]
+    public void AfterCreatioReferenceTestMethod()
+    {
+      Generics<AnyClass> _intInstance = new Generics<AnyClass>();
+      //Assert.AreEqual<int>(0, _intInstance.DefaultValue);
+      Assert.IsNull(_intInstance.DefaultValue);
+    }
+    [TestMethod]
+    public void DictionaryCreatorTestMethod()
+    {
+      //SelfDictionary<AnyClass> _diction = new SelfDictionary<AnyClass>();
+      SelfDictionary<EquatableNotImplemented> _diction = new SelfDictionary<EquatableNotImplemented>();
+    }
+    [TestMethod]
+    [ExpectedException(typeof(NotImplementedException))]
+    public void DictionaryNotImplementedExceptionTestMethod()
+    {
+      SelfDictionary<EquatableNotImplemented> _dictionary = new SelfDictionary<EquatableNotImplemented>();
+      _dictionary.AddIfNotPresent(new EquatableNotImplemented());
+      EquatableNotImplemented _EquatableNotImplementedInstance = new EquatableNotImplemented();
+      _dictionary.AddIfNotPresent(_EquatableNotImplementedInstance);
+      Assert.AreEqual<int>(2, _dictionary.Count);
+      _dictionary.ContainsKey(_EquatableNotImplementedInstance);
+    }
+    private class AnyClass{}
+    private class EquatableNotImplemented : System.IEquatable<EquatableNotImplemented>
+    {
+      public bool Equals(EquatableNotImplemented other)
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+  }
+}
