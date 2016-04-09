de64d02c03dab52de64ea4025ce0fa4b43959d72
Mariusz <mpostol@cas.eu>
2016-04-09 21:28:56 +0200

    #15 done and tested.

---
+++ Lecture/Lecture/ExtensionMethods.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP.Lecture
+{
+  public static class ExtensionMethods
+  {
+
+    public static int WordCount(this String str)
+    {
+      return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+
+    /// <summary>
+    /// Determines whether [contains] [the specified value].
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns><c>true</c> if [contains] [the specified value]; otherwise, <c>false</c>.</returns>
+    /// <exception cref="System.NotImplementedException">Existing method cannot be override</exception>
+    public static bool Contains(this String value)
+    {
+      throw new NotImplementedException("Existing method cannot be override");
+    }
+    public static int MethodA(this IMyInterface myInterface, int i)
+    {
+      return -i;
+    }
+
+    public static string MethodA(this IMyInterface myInterface, string s)
+    {
+      return $"s={s}";
+    }
+  }
+  /// <summary>
+  /// Define an interface named IMyInterface.
+  /// </summary>
+  public interface IMyInterface
+  {
+    // Any class that implements IMyInterface must define a method
+    // that matches the following signature.
+    void MethodB();
+  }
+  //public class A : IMyInterface
+  //{
+
+  //}
+  public class B : IMyInterface
+  {
+    public void MethodB()
+    {
+      throw new NotImplementedException();
+    }
+    public int MethodA(int i) { return -i; }
+  }
+  public class C : IMyInterface
+  {
+    public void MethodB()
+    {
+      throw new NotImplementedException();
+    }
+    public object MethodA(object obj)
+    {
+      return obj;
+    }
+  }
+}
+
---
+++ Lecture/Lesson2UnitTest/ExtensionMethodsUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TP.Lecture.UnitTest
+{
+  [TestClass]
+  public class ExtensionMethodsUnitTest
+  {
+    /// <summary>
+    /// In your code you invoke the extension method with instance method syntax. 
+    /// </summary>
+    [TestMethod]
+    public void InstanceCallTestMethod()
+    {
+      string _TestString = "Hello Extension Methods";
+      Assert.AreEqual<int>(3, _TestString.WordCount()); //To enable extension methods for a particular type, the definition must be visible.
+      Assert.AreEqual<int>(3, ExtensionMethods.WordCount(_TestString)); //Typical method call can also be in use.
+    }
+    /// <summary>
+    /// Mies the test method.
+    /// </summary>
+    [TestMethod]
+    public void OverrideTestMethod()
+    {
+      string _TestString = "Hello Extension Methods";
+      Assert.IsTrue(_TestString.Contains("Hello")); //The existing class method cannot be replaced. An extension method with the same name and signature as an interface or class method will never be called.
+    }
+    [TestMethod]
+    public void MyTestMethod()
+    {
+      // Declare an instance of class B, and class C.
+      B b = new B();
+      C c = new C();
+
+      Assert.AreEqual<int>(-1, b.MethodA(1)); // Extension.MethodA(object, int)
+      string _inputString = "hello";
+      string _expectedResult = "s=hello";
+      Assert.AreEqual<string>(_expectedResult, b.MethodA(_inputString)); // Extension.MethodA(object, string)
+      //Note difference between same and equal.
+      Assert.AreNotSame(_expectedResult, b.MethodA(_inputString)); // Extension.MethodA(object, string)
+
+      // C contains an instance method that matches each of the following method calls.
+      Assert.AreEqual<int>(1, (int)c.MethodA(1));           // C.MethodA(object)
+      Assert.AreSame(_inputString, c.MethodA(_inputString));     // C.MethodA(object)
+      Assert.AreNotSame(new AnyClass(), c.MethodA(new AnyClass()));
+    }
+    private class AnyClass
+    {
+
+    }
+  }
+}
+
