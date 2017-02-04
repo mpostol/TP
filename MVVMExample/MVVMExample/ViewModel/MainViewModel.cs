3a9fdd2602addb6f09b5584ecc5d45f850ae7f16
Mariusz <mpostol@cas.eu>
2017-02-04 09:27:35 +0100

    #23 Added example and UT - UT green.

---
+++ MVVMExample/MVVMExample/ViewModel/MainViewModel.cs
@@ -0,0 +1,100 @@
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+
+namespace TP.MVVMExample.ViewModel
+{
+  /// <summary>
+  /// This class contains properties that the <see cref="MainWindow"/> can data bind to.
+  /// <para>
+  /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+  /// </para>
+  /// <para>
+  /// See http://www.galasoft.ch/mvvm
+  /// </para>
+  /// </summary>
+  public class MainViewModel : ViewModelBase
+  {
+
+    #region constructors
+    /// <summary>
+    /// Initializes a new instance of the MainViewModel class.
+    /// </summary>
+    public MainViewModel()
+    {
+      MyCommand = new RelayCommand(ShowPopupWindow, () => !string.IsNullOrEmpty(m_ActionText));
+      m_ActionText = "Text to be displayed on the popup";
+      Users = new ObservableCollection<User>();
+      Users.Add(new User() { Age = 21, Name = "Jan", Active = true });
+      Users.Add(new User() { Age = 22, Name = "Stefan", Active = false });
+      CurrentUser = Users[0];
+    }
+    #endregion
+
+    #region ViewModel API
+    public ObservableCollection<User> Users
+    {
+      get { return m_Users; }
+      set
+      {
+        m_Users = value;
+        RaisePropertyChanged("Users");
+      }
+    }
+    public User CurrentUser
+    {
+      get
+      {
+        return m_CurrentUser;
+      }
+      set
+      {
+        m_CurrentUser = value;
+        RaisePropertyChanged("CurrentUser");
+      }
+    }
+    public string ActionText
+    {
+      get { return m_ActionText; }
+      set
+      {
+        m_ActionText = value;
+        MyCommand.RaiseCanExecuteChanged();
+        RaisePropertyChanged("ActionText");
+      }
+    }
+    public RelayCommand MyCommand
+    {
+      get;
+      private set;
+    }
+    #endregion
+
+    #region Unit test instrumentation
+    /// <summary>
+    /// Gets or sets the message box show delegate.
+    /// </summary>
+    /// <remarks>
+    /// It is to be used by unit test to override default popup. Limited access ability is addressed by explicate allowing unit test assembly to access internals 
+    /// using <see cref="System.Runtime.CompilerServices.InternalsVisibleToAttribute"/>.
+    /// </remarks>
+    /// <value>The message box show delegate.</value>
+    internal Func<string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult> MessageBoxShowDelegate { get; set; } = MessageBox.Show;
+    #endregion
+
+    #region Private stuff
+    private User m_CurrentUser;
+    private string m_ActionText;
+    private ObservableCollection<User> m_Users;
+    private void ShowPopupWindow()
+    {
+      MessageBoxShowDelegate(ActionText, "Button interaction", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+    #endregion
+
+  }
+}
