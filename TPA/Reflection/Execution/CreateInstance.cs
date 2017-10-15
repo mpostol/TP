0512415803da1c2aae5a56f0878cb163b5fac920
Mariusz <mpostol@cas.eu>
2017-10-15 21:12:40 +0200

    TPA - creation of an object using loosely coupled assemblies  #40 Not tested - UT must be added.

---
+++ TPA/Reflection/Execution/CreateInstance.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace TPA.Reflection.Execution
+{
+    internal static class CreateInstanceFactory
+    {
+
+        internal static Tuple<ITraceSource, Assembly> CreateInstance(string assemblyFile)
+        {
+            FileInfo _pluginFileName = new FileInfo(assemblyFile);
+            if (!_pluginFileName.Exists)
+                throw new FileNotFoundException(nameof(assemblyFile));
+            Assembly _pluginAssembly = Assembly.LoadFrom(_pluginFileName.FullName);
+            return new Tuple<ITraceSource, Assembly>( CreateInstance(_pluginAssembly), _pluginAssembly);
+        }
+        internal static ITraceSource CreateInstance(Assembly pluginAssembly)
+        {
+            if (pluginAssembly == null)
+                throw new NullReferenceException(nameof(pluginAssembly));
+            ITraceSource _serverConfiguration = null;
+            string _iName = typeof(ITraceSource).ToString();
+            foreach (Type pluginType in pluginAssembly.GetExportedTypes())
+                //Only look at public types
+                if (pluginType.IsPublic && !pluginType.IsAbstract && pluginType.GetInterface(_iName) != null)
+                {
+                    _serverConfiguration = (ITraceSource)Activator.CreateInstance(pluginType);
+                    break;
+                }
+            if (_serverConfiguration == null)
+                throw new NullReferenceException(nameof(_serverConfiguration));
+            return _serverConfiguration;
+        }
+    }
+}
