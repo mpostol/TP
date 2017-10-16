ecd7758b7990c2b9695063a2a85593e1b3b3bafe
Mariusz <mpostol@cas.eu>
2017-10-16 20:29:38 +0200

    TPA: Add example for Dynamic Language Runtime  #36 - added dynamic type exemples - UT green.

---
+++ TPA/Reflection/DynamicType/DynamicExampleClass.cs
@@ -0,0 +1,28 @@
+﻿
+namespace TPA.Reflection.DynamicType
+{
+
+    public class DynamicExampleClass
+    {
+
+        public dynamic Increment(dynamic value)
+        {
+            //object vs dynamic differences
+            object _error = 10;
+            //_error += 1; //compiler error
+            dynamic _dyn = 0;
+            _dyn += 1;
+            return value + _dyn;
+        }
+        public dynamic exampleMethod(dynamic d)
+        {
+            dynamic local = "Local variable";
+            int two = 2;
+            if (d is int)
+                return local;
+            else
+                return two;
+        }
+
+    }
+}
---
+++ TPA/ReflectionUnitTest/DynamicType/DynamicExampleClassUnitTest.cs
@@ -0,0 +1,60 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TPA.Reflection.DynamicType;
+
+namespace TPA.Reflection.UnitTest.DynamicType
+{
+    [TestClass]
+    public class DynamicExampleClassUnitTest
+    {
+        [TestMethod]
+        public void ConstructorTestMethod()
+        {
+            DynamicExampleClass _newStrongTypedInstance = new DynamicExampleClass();
+            Assert.IsNotNull(_newStrongTypedInstance);
+            Assert.IsTrue(_newStrongTypedInstance is DynamicExampleClass);
+
+            object _newObjectInstance = new DynamicExampleClass();
+            Assert.IsNotNull(_newObjectInstance);
+            Assert.IsTrue(_newObjectInstance is DynamicExampleClass);
+
+            dynamic _newDynamicInstance = new DynamicExampleClass();
+            Assert.IsNotNull(_newDynamicInstance);
+            Assert.IsTrue(_newDynamicInstance is DynamicExampleClass);
+            //_newDynamicObject.
+        }
+        [TestMethod]
+        public void IncrementIntTest()
+        {
+            DynamicExampleClass _newStrongTypedInstance = new DynamicExampleClass();
+            Assert.AreEqual<int>(11, _newStrongTypedInstance.Increment(10));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderException))]
+        public void IncrementStringTest()
+        {
+            DynamicExampleClass _newStrongTypedInstance = new DynamicExampleClass();
+            Assert.AreEqual<int>(11, _newStrongTypedInstance.Increment("10"));
+        }
+        [TestMethod]
+        public void IncrementDynamicTest()
+        {
+            dynamic _newStrongTypedInstance = new DynamicExampleClass();
+            Assert.AreEqual<int>(11, _newStrongTypedInstance.Increment(10));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderException))]
+        public void IncrementDynamicWrongNumberOfParametersTest()
+        {
+            dynamic _newStrongTypedInstance = new DynamicExampleClass();
+            Assert.AreEqual<int>(11, _newStrongTypedInstance.Increment(10, "10"));
+        }
+        [TestMethod]
+        public void Main()
+        {
+            DynamicExampleClass _ec = new DynamicExampleClass();
+            Assert.AreEqual<int>(2, _ec.exampleMethod("value"));
+            Assert.AreEqual<string>("Local variable", _ec.exampleMethod(10));
+        }
+    }
+}
