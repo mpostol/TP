bcacdc9bf816ffab2fa12dd4a530acd49fa533d1
Szymon Cytrowski <vdrake6@gmail.com>
2017-10-15 18:33:54 -0700

    RX Extensions - proposed another solution for producer and consumer problem

--- TPA/ReactiveProgrammingUnitTest/ProducerConsumerUnitTest.cs
+++ TPA/ReactiveProgrammingUnitTest/ProducerConsumerUnitTest.cs
@@ -16,28 +16,44 @@ namespace ReactiveProgrammingUnitTest
         [TestInitialize]
         public void TestInitialize()
         {
-            // Producer and consumer process setup
-            _producerConsumer = new ProducerConsumer();
-            _producerConsumer.Consumers.Add(new Consumer<int>());
-            _producerConsumer.Consumers.Add(new Consumer<int>());
-            _producerConsumer.Producers.Add(new Producer<int>(TimeSpan.FromSeconds(2)));
-            _producerConsumer.Producers.Add(new Producer<int>(TimeSpan.FromSeconds(3)));
-            _producerConsumer.Producers.Add(new Producer<int>(TimeSpan.FromSeconds(6)));
+            _producer = new Producer<int>(() => ++_counter);
+            _consumer = new Consumer<int>();
+            _producerConsumer = new ProducerConsumer<int>(_producer, _consumer, 2);
         }
 
         [TestMethod]
-        public async Task CheckWhetherConsumersHaveSameResults()
+        public async Task CheckWhetherProducerProducesWell()
         {
-            Consumer<int> firstConsumer = _producerConsumer.Consumers[0];
-            Consumer<int> secondConsumer = _producerConsumer.Consumers[1];
-            // Start asynchronous producer and consumer process
-            await _producerConsumer.StartAsync(TimeSpan.FromSeconds(10));
-            IList<object> firstConsumerResults = firstConsumer.Results;
-            IList<object> secondConsumerResults = secondConsumer.Results;
-            // Check whether consumers have same results
-            Assert.IsTrue(firstConsumerResults.SequenceEqual(secondConsumerResults));
+            List<int> expectedProducedItems = new List<int> { 1, 2, 3 };
+            List<int> producedItems = new List<int>();
+            IObservable<EventPattern<ProducedEventArgs<int>>> producedObservable = Observable
+                .FromEventPattern<ProducedEventArgs<int>>(_producer, "Produced");
+            IDisposable subscriber = producedObservable.Subscribe(e => producedItems.Add(e.EventArgs.Product));
+            _producerConsumer.Start(TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(6));
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            _producerConsumer.Stop();
+            subscriber.Dispose();
+            Assert.IsTrue(expectedProducedItems.SequenceEqual(producedItems));
         }
 
-        private ProducerConsumer _producerConsumer;
+        [TestMethod]
+        public async Task CheckWhetherConsumerConsumesWell()
+        {
+            List<int> expectedConsumedItems = new List<int> { 1, 2, 3 };
+            List<int> consumedItems = new List<int>();
+            IObservable<EventPattern<ConsumedEventArgs<int>>> consumedObservable = Observable
+                .FromEventPattern<ConsumedEventArgs<int>>(_consumer, "Consumed");
+            IDisposable subscriber = consumedObservable.Subscribe(e => consumedItems.Add(e.EventArgs.Product));
+            _producerConsumer.Start(TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(3));
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            _producerConsumer.Stop();
+            subscriber.Dispose();
+            Assert.IsTrue(expectedConsumedItems.SequenceEqual(consumedItems));
+        }
+
+        private int _counter = 0;
+        private Producer<int> _producer;
+        private Consumer<int> _consumer;
+        private ProducerConsumer<int> _producerConsumer;
     }
 }
