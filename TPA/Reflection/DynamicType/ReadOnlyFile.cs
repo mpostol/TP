42bdfe3c71b21f1acde39b96703500e952dc59fa
Mariusz <mpostol@cas.eu>
2017-10-16 21:24:59 +0200

    TPA Using Dynamic Objects Example  #50 - added development anchor.

---
+++ TPA/Reflection/DynamicType/ReadOnlyFile.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Dynamic;
+
+namespace TPA.Reflection.DynamicType
+{
+    public class ReadOnlyFile : DynamicObject
+    {
+        //TODO https://github.com/mpostol/TP/issues/50
+    }
+}
