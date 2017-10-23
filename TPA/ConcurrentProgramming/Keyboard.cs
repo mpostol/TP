90b421caa2cf0191f44752c1aabde97fc0041d5a
HQ\mpostol <mpostol@cas.eu>
2017-10-23 08:03:05 +0200

    TPA Add example for Asynchronous Programming in the .NET Framework   - working on Keybord , tested.

--- TPA/ConcurrentProgramming/Keyboard.cs
+++ TPA/ConcurrentProgramming/Keyboard.cs
@@ -1,110 +1,111 @@
 ﻿
 using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace TPA.AsynchronousBehavior.ConcurrentProgramming
 {
-    public class Keyboard : IDisposable
+  public class Keyboard : IDisposable
+  {
+
+    public Keyboard()
+    {
+      m_Timer = new Timer(x => m_CriticalSection.GenerateChar(), null, 0, 200);
+    }
+
+    #region Task-based Asynchronous Pattern (TAP)
+    public async Task<char> TPAReadKeyFromKeyboardBufferAsync()
+    {
+      return await Task<char>.Run(() => m_CriticalSection.ReadKeyFromKeyboardBuffer());
+    }
+    #endregion
+
+    #region Event-based Asynchronous Pattern (EAP)
+    public event EventHandler<ReadKeyFromKeyboardBufferCompletedEventArgs> ReadKeyFromKeyboardBufferCompleted;
+    public void EAPReadKeyFromKeyboardBufferAsync()
+    {
+      ThreadPool.QueueUserWorkItem(x =>
+          {
+            char _char = m_CriticalSection.ReadKeyFromKeyboardBuffer();
+            ReadKeyFromKeyboardBufferCompleted?.Invoke(this, new ReadKeyFromKeyboardBufferCompletedEventArgs { Result = _char });
+          }
+      );
+    }
+    #endregion
+
+    #region Asynchronous Programming Model  Pattern (APM)
+    public IAsyncResult BeginReadKeyFromKeyboardBuffer(AsyncCallback callback, object parameter)
+    {
+      m_Caller = m_CriticalSection.ReadKeyFromKeyboardBuffer;
+      return m_Caller.BeginInvoke(callback, parameter);
+    }
+    public char EndReadKeyFromKeyboardBuffer(IAsyncResult result)
+    {
+      return m_Caller.EndInvoke(result);
+    }
+    #endregion
+
+    #region IDisposable Support
+    private bool disposedValue = false; // To detect redundant calls
+    protected virtual void Dispose(bool disposing)
+    {
+      if (disposedValue)
+        return;
+      if (disposing)
+      {
+        m_Timer.Dispose();
+        m_CriticalSection.Dispose();
+      }
+      disposedValue = true;
+    }
+    // This code added to correctly implement the disposable pattern.
+    public void Dispose()
+    {
+      // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+      Dispose(true);
+    }
+    #endregion
+
+    #region private
+    private delegate char AsyncMethodCaller();
+    private Timer m_Timer;
+    private AsyncMethodCaller m_Caller;
+    private CriticalSection m_CriticalSection = new CriticalSection();
+
+    [Synchronization(true)]
+    class CriticalSection : IDisposable
     {
 
-        public event EventHandler<ReadKeyFromKeyboardBufferCompletedEventArgs> ReadKeyFromKeyboardBufferCompleted;
-        public Keyboard()
-        {
-            m_Timer = new Timer(GenerateChar, m_AutoResetEvent, 1500, 200);
-        }
+      internal void GenerateChar()
+      {
+        _charBuffer.Enqueue((char)m_Random.Next('a', 'z'));
+        if (_charBuffer.Count == 1)
+          m_AutoResetEvent.Set();
+      }
+      internal char ReadKeyFromKeyboardBuffer()
+      {
+        if (_charBuffer.Count == 0)
+          m_AutoResetEvent.WaitOne(-1, true);
+        else if (_charBuffer.Count == 1)
+          m_AutoResetEvent.Reset();
+        Debug.Assert(_charBuffer.Any<Char>());
+        return _charBuffer.Dequeue();
+      }
 
-        public Task<char> ReadKeyFromKeyboardBufferAsync()
-        {
-            TaskCompletionSource<char> _tcs = new TaskCompletionSource<char>();
-            m_AutoResetEvent.WaitOne();
-            Debug.Assert(_charBuffer.Any<Char>());
-
-            RegisteredWaitHandle _waitHandle = ThreadPool.RegisterWaitForSingleObject(m_AutoResetEvent, (state, timedOut) =>
-            {
-                _charBuffer.TryDequeue(out char _result);
-                _tcs.SetResult(_result);
-            }, null, Timeout.Infinite, true);
-
-            Task<char> _task = _tcs.Task;
-            _task.ContinueWith(task => _waitHandle.Unregister(null));
-            return _task;
-        }
-        public void ReadKeyFromKeyboardBufferAsyncUsingEAP()
-        {
-            char _result = default;
-            AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
-            void _Callback(object state)
-            {
-                try
-                {
-                    _result = ReadKeyFromKeyboardBuffer();
-                }
-                finally
-                {
-                    _autoResetEvent.Set();
-                }
-            }
-            ThreadPool.QueueUserWorkItem(_Callback);
-            _autoResetEvent.WaitOne();
-            _autoResetEvent.Close();
-            ReadKeyFromKeyboardBufferCompleted?.Invoke(this, new ReadKeyFromKeyboardBufferCompletedEventArgs { Result = _result });
-        }
-        public IAsyncResult BeginReadKeyFromKeyboardBuffer(AsyncCallback callback, object parameter)
-        {
-            m_Caller = ReadKeyFromKeyboardBuffer;
-            return m_Caller.BeginInvoke(callback, parameter);
-        }
-        public char EndReadKeyFromKeyboardBuffer(IAsyncResult result)
-        {
-            return m_Caller.EndInvoke(result);
-        }
-        private char ReadKeyFromKeyboardBuffer()
-        {
-            m_AutoResetEvent.WaitOne();
-            Debug.Assert(_charBuffer.Any<Char>());
-            _charBuffer.TryDequeue(out char result);
-            return result;
-        }
-
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposedValue)
-                return;
-            if (disposing)
-            {
-                m_Timer.Dispose();
-                m_AutoResetEvent.Close();
-            }
-            disposedValue = true;
-        }
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-        }
-        #endregion
-
-        #region private
-        private delegate char AsyncMethodCaller();
-        private readonly ConcurrentQueue<char> _charBuffer = new ConcurrentQueue<char>();
-        private readonly AutoResetEvent m_AutoResetEvent = new AutoResetEvent(false);
-        private Timer m_Timer;
-        private Random m_Random = new Random();
-        private AsyncMethodCaller m_Caller;
-
-        private void GenerateChar(object state)
-        {
-            _charBuffer.Enqueue((char)m_Random.Next('a', 'z'));
-            m_AutoResetEvent.Set();
-        }
-        #endregion
-
+      public void Dispose()
+      {
+        m_AutoResetEvent.Dispose();
+      }
+      private readonly Queue<char> _charBuffer = new Queue<char>();
+      private Random m_Random = new Random();
+      private readonly AutoResetEvent m_AutoResetEvent = new AutoResetEvent(false);
     }
+
+    #endregion    
+
+  }
 }
--- TPA/ConcurrentProgrammingUnitTest/KeyboardUnitTest.cs
+++ TPA/ConcurrentProgrammingUnitTest/KeyboardUnitTest.cs
@@ -7,62 +7,77 @@ using TPA.AsynchronousBehavior.ConcurrentProgramming;
 
 namespace ConcurrentProgrammingUnitTest
 {
-    [TestClass]
-    public class KeyboardUnitTest
+  [TestClass]
+  public class KeyboardUnitTest
+  {
+
+    [TestMethod]
+    public void CheckWhetherKeyboardGeneratesKeystrokes()
     {
-
-        [TestMethod]
-        public async Task CheckWhetherKeyboardGeneratesKeystrokes()
+      using (Keyboard _keyboard = new Keyboard())
+      {
+        List<char> _chars = new List<char>();
+        long _counter = 0;
+        for (int i = 0; i < 3; i++)
         {
-            using (Keyboard _keyboard = new Keyboard())
-            {
-                List<char> _chars = new List<char>();
-                for (int i = 0; i < 3; i++)
-                    _chars.Add(await _keyboard.ReadKeyFromKeyboardBufferAsync());
-                Assert.IsTrue(_chars.Count == 3);
-            }
+          Task<char> _workToDo = _keyboard.TPAReadKeyFromKeyboardBufferAsync();
+          while (_workToDo.Status != TaskStatus.RanToCompletion)
+          {
+            _counter++;
+          };
+          char _lastChar = _workToDo.Result;
+          _chars.Add(_lastChar);
         }
-
-        [TestMethod]
-        public void CheckWhetherKeyboardGeneratesKeystrokesUsingAPM()
-        {
-            using (Keyboard _keyboard = new Keyboard())
-            {
-                List<char> _chars = new List<char>();
-
-                for (int i = 0; i < 3; i++)
-                {
-                    IAsyncResult asyncResult = _keyboard.BeginReadKeyFromKeyboardBuffer(null, null);
-                    _chars.Add(_keyboard.EndReadKeyFromKeyboardBuffer(asyncResult));
-                }
-
-                Assert.IsTrue(_chars.Count == 3);
-            }
-        }
-
-        [TestMethod]
-        public async Task CheckWhetherKeyboardGeneratesKeystrokesUsingEAP()
-        {
-            using (Keyboard _keyboard = new Keyboard())
-            {
-                List<char> _chars = new List<char>();
-                SemaphoreSlim semaphore = new SemaphoreSlim(0);
-
-                _keyboard.ReadKeyFromKeyboardBufferCompleted += (sender, args) =>
-                {
-                    _chars.Add(args.Result);
-                    semaphore.Release();
-                };
-
-                for (int i = 0; i < 3; i++)
-                {
-                    _keyboard.ReadKeyFromKeyboardBufferAsyncUsingEAP();
-                    await semaphore.WaitAsync();
-                }
-
-                Assert.IsTrue(_chars.Count == 3);
-            }
-
-        }
+        Assert.AreEqual<int>(3, _chars.Count);
+        Assert.IsTrue(_counter > 1000000);
+      }
     }
+
+    [TestMethod]
+    public void CheckWhetherKeyboardGeneratesKeystrokesUsingAPM()
+    {
+      using (Keyboard _keyboard = new Keyboard())
+      {
+        List<char> _chars = new List<char>();
+        long _counter = 0;
+        for (int i = 0; i < 3; i++)
+        {
+          bool _ended = false;
+          IAsyncResult asyncResult = _keyboard.BeginReadKeyFromKeyboardBuffer(x => _ended = true, null);
+          while (!_ended)
+          {
+            _counter++;
+          };
+          _chars.Add(_keyboard.EndReadKeyFromKeyboardBuffer(asyncResult));
+        }
+        Assert.AreEqual<int>(3, _chars.Count);
+        Assert.IsTrue(_counter > 1000000);
+      }
+    }
+
+    [TestMethod]
+    public async Task CheckWhetherKeyboardGeneratesKeystrokesUsingEAP()
+    {
+      using (Keyboard _keyboard = new Keyboard())
+      {
+        List<char> _chars = new List<char>();
+        SemaphoreSlim semaphore = new SemaphoreSlim(0);
+
+        _keyboard.ReadKeyFromKeyboardBufferCompleted += (sender, args) =>
+        {
+          _chars.Add(args.Result);
+          semaphore.Release();
+        };
+
+        for (int i = 0; i < 3; i++)
+        {
+          _keyboard.EAPReadKeyFromKeyboardBufferAsync();
+          await semaphore.WaitAsync();
+        }
+
+        Assert.IsTrue(_chars.Count == 3);
+      }
+
+    }
+  }
 }
