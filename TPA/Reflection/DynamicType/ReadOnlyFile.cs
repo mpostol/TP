26c3fdc5ac0b90a868444d36ecbe1b48c2f9ef20
MAKOLAB-NET\mariusz.postol <mariusz.postol@makolab.com>
2017-10-24 22:35:56 +0200

    TPA Using Dynamic Objects Example #50  - added  - added TryGetMember.

--- TPA/Reflection/DynamicType/ReadOnlyFile.cs
+++ TPA/Reflection/DynamicType/ReadOnlyFile.cs
@@ -6,5 +6,12 @@ namespace TPA.Reflection.DynamicType
   public class ReadOnlyFile : DynamicObject
   {
     //TODO https://github.com/mpostol/TP/issues/50
+    public override bool TryGetMember(GetMemberBinder binder, out object result)
+    {
+      result = null;
+      //TODO to be implemented.
+      return true;
+    }
+
   }
 }
