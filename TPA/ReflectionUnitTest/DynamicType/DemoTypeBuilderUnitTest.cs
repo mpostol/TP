c0023801f5eff08ac264001f74c6186a2a1e3e2d
szandrzejczak <203847@edu.p.lodz.pl>
2017-11-27 22:28:19 +0100

    https://github.com/mpostol/TP/issues/59 Added starting class and unit test

---
+++ TPA/ReflectionUnitTest/DynamicType/DemoTypeBuilderUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TPA.Reflection.UnitTest.DynamicType
+{
+    [TestClass]
+    public class DemoTypeBuilderUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
