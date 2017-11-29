179cf8d56809f0e5043fe9f8be6ebd281ebbd1af
szandrzejczak <203847@edu.p.lodz.pl>
2017-11-29 01:07:44 +0100

    https://github.com/mpostol/TP/issues/59 Added examples with non-default constructor, property, private field and public method

--- TPA/Reflection/DynamicType/DemoTypeBuilder.cs
+++ TPA/Reflection/DynamicType/DemoTypeBuilder.cs
@@ -10,19 +10,19 @@ namespace TPA.Reflection.DynamicType
 {
     public static class DemoTypeBuilder
     {
-        public static object createInstanceWithPublicField()
+        public static object CreateInstanceWithPublicField()
         {
             //specify name of dynamic assembly
             AssemblyName assemblyName = new AssemblyName("DemoAssembly");
-            //create dynamic assembly
+            //define dynamic assembly
             AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
-            //create module for specified assembly
+            //define module for specified assembly
             ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name, assemblyName.Name + ".dll");
-            //create a public type in specified module
+            //define a public type in specified module
             TypeBuilder typeBuilder = moduleBuilder.DefineType("DemoType", TypeAttributes.Public);
-            //create a public field in specified type
-            FieldBuilder fieldBuilder = typeBuilder.DefineField("number", typeof(int), FieldAttributes.Public);
-            //load Type object for created type
+            //define a public field in specified type
+            FieldBuilder fieldBuilder = typeBuilder.DefineField("m_number", typeof(int), FieldAttributes.Public);
+            //load Type object for defined type
             Type type = typeBuilder.CreateType();
             //save assembly on disk
             assemblyBuilder.Save(assemblyName.Name + ".dll");
@@ -30,37 +30,115 @@ namespace TPA.Reflection.DynamicType
             return Activator.CreateInstance(type);
         }
 
-        public static object createInstanceWithPublicFieldAndDefaultConstructor()
+        public static object CreateInstanceWithPublicFieldAndDefaultConstructor()
         {
             AssemblyName assemblyName = new AssemblyName("DemoAssembly2");
             AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
             ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name, assemblyName.Name + ".dll");
             TypeBuilder typeBuilder = moduleBuilder.DefineType("DemoType2", TypeAttributes.Public);
-            FieldBuilder fieldBuilder = typeBuilder.DefineField("number", typeof(int), FieldAttributes.Public);
-            
-            Type[] parameterTypes = { typeof(int) };
-            ConstructorBuilder ctor1 = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, parameterTypes);
+            FieldBuilder fieldBuilder = typeBuilder.DefineField("m_number", typeof(int), FieldAttributes.Public);
+
+            //define default constructor
+            ConstructorBuilder ctor1 = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, Type.EmptyTypes);
             ILGenerator ctor1IL = ctor1.GetILGenerator();
-            //push reference to the new instance on stack
+            //push reference to the new instance on the stack
             ctor1IL.Emit(OpCodes.Ldarg_0);
+            //call the base class constructor
             ctor1IL.Emit(OpCodes.Call, typeof(object).GetConstructor(Type.EmptyTypes));
-            //push the instance on stack
+            //push the instance on the stack
             ctor1IL.Emit(OpCodes.Ldarg_0);
-            //push the argument on stack
-            ctor1IL.Emit(OpCodes.Ldarg_1);
+            //push the default field value on the stack
+            ctor1IL.Emit(OpCodes.Ldc_I4_S, 7);
             //assign pushed value to the field
             ctor1IL.Emit(OpCodes.Stfld, fieldBuilder);
             ctor1IL.Emit(OpCodes.Ret);
 
-            ConstructorBuilder ctor2 = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, Type.EmptyTypes);
-            ILGenerator ctor2IL = ctor2.GetILGenerator();
-            //push reference to the new instance on stack
-            ctor2IL.Emit(OpCodes.Ldarg_0);
-            //push default valueof the field
-            ctor2IL.Emit(OpCodes.Ldc_I4_S, 7);
-            //call parametrized contructor ctor1
-            ctor2IL.Emit(OpCodes.Call, ctor1);
-            ctor2IL.Emit(OpCodes.Ret);
+            Type type = typeBuilder.CreateType();
+            assemblyBuilder.Save(assemblyName.Name + ".dll");
+            return Activator.CreateInstance(type);
+        }
+
+        public static object CreateInstanceWithNonDefaultConstructorAndPublicPropertyAndPrivateField()
+        {
+            AssemblyName assemblyName = new AssemblyName("DemoAssembly3");
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name, assemblyName.Name + ".dll");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("DemoType3", TypeAttributes.Public);
+            FieldBuilder fieldBuilder = typeBuilder.DefineField("m_number", typeof(int), FieldAttributes.Private);
+
+            //define a non-default constructor with parameters
+            Type[] parameterTypes = { typeof(int) };
+            ConstructorBuilder ctor1 = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, parameterTypes);
+            ILGenerator ctor1IL = ctor1.GetILGenerator();
+            ctor1IL.Emit(OpCodes.Ldarg_0);
+            ctor1IL.Emit(OpCodes.Call, typeof(object).GetConstructor(Type.EmptyTypes));
+            ctor1IL.Emit(OpCodes.Ldarg_0);
+            //push the provided argument value on the stack
+            ctor1IL.Emit(OpCodes.Ldarg_1);
+            ctor1IL.Emit(OpCodes.Stfld, fieldBuilder);
+            ctor1IL.Emit(OpCodes.Ret);
+
+            //define a property named MyNumber which gets and sets the private field m_number
+            PropertyBuilder propertyBuilder = typeBuilder.DefineProperty("MyNumber", PropertyAttributes.HasDefault, typeof(int), null);
+            //define the getter and setter methods
+            MethodAttributes attributes = MethodAttributes.Public |
+                MethodAttributes.SpecialName | MethodAttributes.HideBySig;
+            MethodBuilder getterMethodBuilder = typeBuilder.DefineMethod(
+                "get_MyNumber",
+                attributes,
+                typeof(int),
+                Type.EmptyTypes);
+            ILGenerator getterIL = getterMethodBuilder.GetILGenerator();
+            getterIL.Emit(OpCodes.Ldarg_0);
+            //push the field value to the stack
+            getterIL.Emit(OpCodes.Ldfld, fieldBuilder);
+            //return the field value which has been pushed to the stack
+            getterIL.Emit(OpCodes.Ret);
+            MethodBuilder setterMethodBuilder = typeBuilder.DefineMethod(
+                "set_MyNumber",
+                attributes,
+                null,
+                new Type[] { typeof(int) });
+            ILGenerator setterIL = setterMethodBuilder.GetILGenerator();
+            setterIL.Emit(OpCodes.Ldarg_0);
+            //push the argument value to the stack
+            setterIL.Emit(OpCodes.Ldarg_1);
+            //store the pushed value in the field
+            setterIL.Emit(OpCodes.Stfld, fieldBuilder);
+            setterIL.Emit(OpCodes.Ret);
+            //set the getter and setter methods
+            propertyBuilder.SetGetMethod(getterMethodBuilder);
+            propertyBuilder.SetSetMethod(setterMethodBuilder);
+
+
+            Type type = typeBuilder.CreateType();
+            assemblyBuilder.Save(assemblyName.Name + ".dll");
+            return Activator.CreateInstance(type, new object[] { 66 });
+        }
+
+        public static object CreateInstanceWithPublicMethod()
+        {
+            AssemblyName assemblyName = new AssemblyName("DemoAssembly4");
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name, assemblyName.Name + ".dll");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("DemoType4", TypeAttributes.Public);
+
+            //define a public method which returns its integer argument incremented
+            MethodBuilder methodBuilder = typeBuilder.DefineMethod(
+                "MyMethod",
+                MethodAttributes.Public,
+                typeof(int),
+                new Type[] {typeof(int)});
+            ILGenerator myMethodIL = methodBuilder.GetILGenerator();
+            //push the argument value on the stack
+            myMethodIL.Emit(OpCodes.Ldarg_1);
+            //push the value 1 on the stack
+            myMethodIL.Emit(OpCodes.Ldc_I4_S, 1);
+            //add these two values
+            myMethodIL.Emit(OpCodes.Add);
+            //return the result
+            myMethodIL.Emit(OpCodes.Ret);
+            
 
             Type type = typeBuilder.CreateType();
             assemblyBuilder.Save(assemblyName.Name + ".dll");
--- TPA/ReflectionUnitTest/DynamicType/DemoTypeBuilderUnitTest.cs
+++ TPA/ReflectionUnitTest/DynamicType/DemoTypeBuilderUnitTest.cs
@@ -11,14 +11,14 @@ namespace TPA.Reflection.UnitTest.DynamicType
         [TestMethod]
         public void TestCreateInstanceWithPublicField()
         {
-            object instance = DemoTypeBuilder.createInstanceWithPublicField();
+            object instance = DemoTypeBuilder.CreateInstanceWithPublicField();
             Assert.IsNotNull(instance);
 
             Type type = instance.GetType();
             Assert.IsNotNull(type);
             Assert.IsTrue(type.Name == "DemoType");
-            
-            FieldInfo fieldInfo = type.GetField("number");
+
+            FieldInfo fieldInfo = type.GetField("m_number");
             Assert.IsNotNull(fieldInfo.GetValue(instance));
             Assert.AreEqual(fieldInfo.GetValue(instance).GetType(), typeof(int));
             Assert.AreEqual(fieldInfo.GetValue(instance), 0);
@@ -27,22 +27,54 @@ namespace TPA.Reflection.UnitTest.DynamicType
         [TestMethod]
         public void TestCreateInstanceWithPublicFieldAndDefaultConstructor()
         {
-            object instance = DemoTypeBuilder.createInstanceWithPublicFieldAndDefaultConstructor();
-            Assert.IsNotNull(instance);
+            object instance = DemoTypeBuilder.CreateInstanceWithPublicFieldAndDefaultConstructor();
 
             Type type = instance.GetType();
             Assert.IsNotNull(type);
             Assert.IsTrue(type.Name == "DemoType2");
 
-            FieldInfo fieldInfo = type.GetField("number");
+            FieldInfo fieldInfo = type.GetField("m_number");
             Assert.IsNotNull(fieldInfo.GetValue(instance));
             Assert.AreEqual(fieldInfo.GetValue(instance).GetType(), typeof(int));
             Assert.AreEqual(fieldInfo.GetValue(instance), 7);
+        }
 
-            instance = Activator.CreateInstance(type, 21);
-            Assert.IsNotNull(fieldInfo.GetValue(instance));
-            Assert.AreEqual(fieldInfo.GetValue(instance).GetType(), typeof(int));
-            Assert.AreEqual(fieldInfo.GetValue(instance), 21);
+        [TestMethod]
+        public void TestCreateInstanceWithNonDefaultConstructorAndPublicPropertyAndPrivateField()
+        {
+            object instance = DemoTypeBuilder.CreateInstanceWithNonDefaultConstructorAndPublicPropertyAndPrivateField();
+            Assert.IsNotNull(instance);
+
+            Type type = instance.GetType();
+            Assert.IsNotNull(type);
+            Assert.IsTrue(type.Name == "DemoType3");
+
+            PropertyInfo propertyInfo = type.GetProperty("MyNumber");
+            Assert.IsNotNull(propertyInfo);
+            int res = (int) propertyInfo.GetValue(instance);
+            Assert.AreEqual(typeof(int), res.GetType());
+            Assert.AreEqual(66, res);
+            propertyInfo.SetValue(instance, 77);
+            res = (int)propertyInfo.GetValue(instance);
+            Assert.AreEqual(typeof(int), res.GetType());
+            Assert.AreEqual(77, res);
+        }
+
+        [TestMethod]
+        public void TestCreateInstanceWithPublicMethod()
+        {
+            object instance = DemoTypeBuilder.CreateInstanceWithPublicMethod();
+            Assert.IsNotNull(instance);
+
+            Type type = instance.GetType();
+            Assert.IsNotNull(type);
+            Assert.IsTrue(type.Name == "DemoType4");
+
+            MethodInfo methodInfo = type.GetMethod("MyMethod");
+            Assert.IsNotNull(methodInfo);
+            int res = (int)methodInfo.Invoke(instance, new object[] { 4 });
+            Assert.AreEqual(typeof(int), res.GetType());
+            Assert.AreEqual(5, res);
         }
     }
 }
