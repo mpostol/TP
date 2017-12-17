8578c612a6f09affbed60bba5d2aa4eb63388364
mariusz postol <mpostol@cas.eu>
2017-12-17 15:31:08 +0100

    TPA Add project containing examples related to application configuration adaptability. #69 - done and tested.

---
+++ TPA/TPA.ConfigurationUnitTest/SettingsUnitTest.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TPA.Configuration.UnitTest
+{
+  [TestClass]
+  public class SettingsUnitTest
+  {
+    [TestMethod]
+    public void SettingsTest()
+    {
+      Assert.AreEqual<String>("SettingString", Properties.Settings.Default.SettingString);
+      Assert.AreEqual<Color>(Color.Red, Properties.Settings.Default.SettingColor);
+    }
+  }
+}
