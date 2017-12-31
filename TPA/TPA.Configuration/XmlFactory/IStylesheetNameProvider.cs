231ca6f37c59c68bc9677292f28052aa3ea85959
mariusz postol <mpostol@cas.eu>
2017-12-31 17:19:30 +0100

    Add external file xml configuration. #70 - added xml serialization/deserialization.

---
+++ TPA/TPA.Configuration/XmlFactory/IStylesheetNameProvider.cs
@@ -0,0 +1,16 @@
+﻿
+namespace TPA.Configuration.XmlFactory
+{
+  
+  /// <summary>
+  /// Represents XML file style sheet name provider
+  /// </summary>
+  public interface IStylesheetNameProvider
+  {
+    /// <summary>
+    /// The style sheet name
+    /// </summary>
+    string StylesheetNmane { get; }
+
+  }
+}
