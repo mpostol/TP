1c2ca5825cb3fe0dae259c3a8b2f1f1d3add5837
MPostol <mpostol@cas.eu>
2018-06-04 08:48:50 +0200

    Generics review the code and UT #79 - added new example NodeObservable.cs

---
+++ Lecture/Lecture/LessonGenerics/NodeObservable.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TP.Lecture.LessonGenerics
+{
+  public class NodeEnumerable<TypeParameter> : IEnumerable<TypeParameter>
+  {
+    public NodeEnumerable<TypeParameter> Next { get; private set; }
+    public static NodeEnumerable<TypeParameter> First { get; private set; } = null;
+    public TypeParameter Value { get; private set; }
+    public NodeEnumerable(TypeParameter value)
+    {
+      Value = value;
+      Next = First;
+      First = this;
+    }
+
+    #region IEnumerable<TypeParameter>
+    public IEnumerator<TypeParameter> GetEnumerator()
+    {
+      NodeEnumerable<TypeParameter> _current = First;
+      while (_current != null)
+      {
+        TypeParameter _value = _current.Value;
+        _current = _current.Next;
+        yield return _value;
+      }
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return this.GetEnumerator();
+    }
+    #endregion
+
+  }
+
+}
