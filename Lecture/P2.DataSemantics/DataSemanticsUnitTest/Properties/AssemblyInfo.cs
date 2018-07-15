5915f8b2576cfaff46845ccb48b7fadd2736ca03
MPostol <mpostol@cas.eu>
2018-07-15 15:13:24 +0200

    The code from `Lecture/Lecture` must be moved consequently #95 - !! working copy !!
    - rebuilding the folder structure next step

---
+++ Lecture/P2.DataSemantics/DataSemanticsUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataSemanticsUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataSemanticsUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c76d1a8-924f-4a93-8000-d5c94fd65f42")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
