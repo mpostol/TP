362485fe3e64c5e69ab29f480551c67579d309bd
MPostol <mpostol@cas.eu>
2018-07-17 13:19:43 +0200

    TPA Using Dynamic Objects Example fixes #50

--- TPA/Reflection/DynamicType/ReadOnlyFile.cs
+++ TPA/Reflection/DynamicType/ReadOnlyFile.cs
@@ -1,4 +1,10 @@
-﻿
+﻿//____________________________________________________________________________
+//
+//  Copyright (C) 2018, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
@@ -6,112 +12,89 @@ using System.IO;
 
 namespace TPA.Reflection.DynamicType
 {
-    public enum StringSearchOption
+  public enum StringSearchOption { Contains, StartsWith, EndsWith }
+
+  public class ReadOnlyFile : DynamicObject
+  {
+
+    public ReadOnlyFile(string filePath)
     {
-        Contains,
-        StartsWith,
-        EndsWith
+      if (!File.Exists(filePath))
+        throw new FileLoadException("File path does not exist.");
+      p_filePath = filePath;
+    }
+    public override bool TryGetMember(GetMemberBinder binder, out object result)
+    {
+      result = GetPropertyValue(binder.Name);
+      return result == null ? false : true;
+    }
+    public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+    {
+      StringSearchOption _stringSearchOption = StringSearchOption.StartsWith;
+      bool trimSpaces = true;
+      try
+      {
+        if (args.Length > 0)
+          _stringSearchOption = (StringSearchOption)args[0];
+      }
+      catch
+      {
+        throw new ArgumentException("StringSearchOption argument must be a StringSearchOption enum value.");
+      }
+      try
+      {
+        if (args.Length > 1)
+          trimSpaces = (bool)args[1];
+      }
+      catch
+      {
+        throw new ArgumentException("trimSpaces argument must be a Boolean value.");
+      }
+      result = GetPropertyValue(binder.Name, _stringSearchOption, trimSpaces);
+      return result == null ? false : true;
     }
 
-    public class ReadOnlyFile : DynamicObject
+    private string p_filePath;
+    private IEnumerable<string> GetPropertyValue(string propertyName, StringSearchOption StringSearchOption = StringSearchOption.StartsWith, bool trimSpaces = true)
     {
-        private string p_filePath;
-        
-        public ReadOnlyFile(string filePath)
+      StreamReader _sr = null;
+      List<string> _results = new List<string>();
+      string _line = "";
+      string _testLine = "";
+      try
+      {
+        _sr = new StreamReader(p_filePath);
+        while (!_sr.EndOfStream)
         {
-            if (!File.Exists(filePath))
-            {
-                throw new FileLoadException("File path does not exist.");
-            }
-
-            p_filePath = filePath;
+          _line = _sr.ReadLine();
+          _testLine = _line.ToUpper();
+          if (trimSpaces)
+            _testLine = _testLine.Trim();
+          switch (StringSearchOption)
+          {
+            case StringSearchOption.StartsWith:
+              if (_testLine.StartsWith(propertyName.ToUpper())) { _results.Add(_line); }
+              break;
+            case StringSearchOption.Contains:
+              if (_testLine.Contains(propertyName.ToUpper())) { _results.Add(_line); }
+              break;
+            case StringSearchOption.EndsWith:
+              if (_testLine.EndsWith(propertyName.ToUpper())) { _results.Add(_line); }
+              break;
+          }
         }
-        
-        public override bool TryGetMember(GetMemberBinder binder, out object result)
-        {           
-            result = GetPropertyValue(binder.Name);
-            return result == null ? false : true;
-        }
-        
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
-        {
-            StringSearchOption StringSearchOption = StringSearchOption.StartsWith;
-            bool trimSpaces = true;
-
-            try
-            {
-                if (args.Length > 0) { StringSearchOption = (StringSearchOption)args[0]; }
-            }
-            catch
-            {
-                throw new ArgumentException("StringSearchOption argument must be a StringSearchOption enum value.");
-            }
-
-            try
-            {
-                if (args.Length > 1)
-                {
-                    trimSpaces = (bool)args[1];
-                }
-            }
-            catch
-            {
-                throw new ArgumentException("trimSpaces argument must be a Boolean value.");
-            }
-
-            result = GetPropertyValue(binder.Name, StringSearchOption, trimSpaces);
-
-            return result == null ? false : true;
-        }
-
-        private IEnumerable<string> GetPropertyValue(string propertyName, StringSearchOption StringSearchOption = StringSearchOption.StartsWith, bool trimSpaces = true)
-        {
-            StreamReader sr = null;
-            List<string> results = new List<string>();
-            string line = "";
-            string testLine = "";
-
-            try
-            {
-                sr = new StreamReader(p_filePath);
-
-                while (!sr.EndOfStream)
-                {
-                    line = sr.ReadLine();
-                    
-                    testLine = line.ToUpper();
-                    if (trimSpaces)
-                    {
-                        testLine = testLine.Trim();
-                    }
-
-                    switch (StringSearchOption)
-                    {
-                        case StringSearchOption.StartsWith:
-                            if (testLine.StartsWith(propertyName.ToUpper())) { results.Add(line); }
-                            break;
-                        case StringSearchOption.Contains:
-                            if (testLine.Contains(propertyName.ToUpper())) { results.Add(line); }
-                            break;
-                        case StringSearchOption.EndsWith:
-                            if (testLine.EndsWith(propertyName.ToUpper())) { results.Add(line); }
-                            break;
-                    }
-                }
-            }
-            catch
-            {
-                results = null;
-            }
-            finally
-            {
-                if (sr != null)
-                {
-                    sr.Close();
-                }
-            }
-
-            return results;
-        }
+      }
+      catch
+      {
+        _results = null;
+      }
+      finally
+      {
+        if (_sr != null)
+          _sr.Close();
+      }
+      return _results;
     }
+
+  }
 }
--- TPA/ReflectionUnitTest/DynamicType/ReadOnlyFileUnitTest.cs
+++ TPA/ReflectionUnitTest/DynamicType/ReadOnlyFileUnitTest.cs
@@ -1,19 +1,57 @@
-﻿using System;
+﻿//____________________________________________________________________________
+//
+//  Copyright (C) 2018, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using TPA.Reflection.DynamicType;
 
 namespace TPA.Reflection.UnitTest.DynamicType
 {
-    [TestClass]
-    public class ReadOnlyFileUnitTest
-    {
-        private string TestFilePath => "ReadOnlyFileTest.txt";
+  [TestClass]
+  public class ReadOnlyFileUnitTest
+  {
 
-        private void InitFile()
-        {
-            #region File Content
-            string content = @"List of customers and suppliers  
+    [TestMethod]
+    [ExpectedException(typeof(FileLoadException))]
+    public void CtorNotExistingFileTest()
+    {
+      ReadOnlyFile _readOnlyFile = new ReadOnlyFile("$$$.%%%");
+    }
+    [TestMethod]
+    public void ContainsPropertyNameTest()
+    {
+      InitFile();
+      dynamic _rFile = new ReadOnlyFile(TestFilePath);
+      foreach (string line in _rFile.Customer(StringSearchOption.Contains, true))
+        Assert.IsTrue(line.ToUpper().Contains("Customer".ToUpper()));
+    }
+    [TestMethod]
+    public void StartsWithPropertyNameTest()
+    {
+      InitFile();
+      dynamic _rFile = new ReadOnlyFile(TestFilePath);
+      foreach (string line in _rFile.Supplier(StringSearchOption.StartsWith, true))
+        Assert.IsTrue(line.ToUpper().StartsWith("Supplier".ToUpper()));
+    }
+    [TestMethod]
+    public void EndsWithPropertyNameTest()
+    {
+      InitFile();
+      dynamic _rFile = new ReadOnlyFile(TestFilePath);
+      foreach (string line in _rFile.Patrick(StringSearchOption.StartsWith, true))
+        Assert.IsTrue(line.ToUpper().EndsWith("Patrick".ToUpper()));
+    }
+
+    #region test instrumentation
+    private string TestFilePath => "ReadOnlyFileTest.txt";
+    private void InitFile()
+    {
+      #region File Content
+      string content = @"List of customers and suppliers  
 Supplier: Lucerne Publishing (http://www.lucernepublishing.com/)  
 Customer: Preston, Chris  
 Customer: Hines, Patrick  
@@ -23,53 +61,12 @@ Supplier: Fabrikam, Inc. (http://www.fabrikam.com/)
 Customer: Seubert, Roxanne  
 Supplier: Proseware, Inc. (http://www.proseware.com/)   
 Customer: Adolphi, Stephan  
-Customer: Koch, Paul  "; 
-            #endregion
-
-            if (!File.Exists(TestFilePath) || File.ReadAllText(TestFilePath) != content)
-            {
-                File.WriteAllText(TestFilePath, content);
-            }
-        }
-        
-        [TestMethod]
-        [ExpectedException(typeof(FileLoadException))]
-        public void CtorNotExistingFileTest()
-        {
-            var ReadOnlyFile = new ReadOnlyFile("$$$.%%%");
-        }
-
-        [TestMethod]
-        public void ContainsPropertyNameTest()
-        {
-            InitFile();
-            dynamic rFile = new ReadOnlyFile(TestFilePath);
-            foreach (string line in rFile.Customer(StringSearchOption.Contains, true))
-            {
-                Assert.IsTrue(line.ToUpper().Contains("Customer".ToUpper()));
-            }
-        }
-
-        [TestMethod]
-        public void StartsWithPropertyNameTest()
-        {
-            InitFile();
-            dynamic rFile = new ReadOnlyFile(TestFilePath);
-            foreach (string line in rFile.Supplier(StringSearchOption.StartsWith, true))
-            {
-                Assert.IsTrue(line.ToUpper().StartsWith("Supplier".ToUpper()));
-            }
-        }
-
-        [TestMethod]
-        public void EndsWithPropertyNameTest()
-        {
-            InitFile();
-            dynamic rFile = new ReadOnlyFile(TestFilePath);
-            foreach (string line in rFile.Patrick(StringSearchOption.StartsWith, true))
-            {
-                Assert.IsTrue(line.ToUpper().EndsWith("Patrick".ToUpper()));
-            }
-        }
+Customer: Koch, Paul  ";
+      #endregion
+      if (!File.Exists(TestFilePath) || File.ReadAllText(TestFilePath) != content)
+        File.WriteAllText(TestFilePath, content);
     }
+    #endregion
+
+  }
 }
