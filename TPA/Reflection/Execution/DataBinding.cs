c1fd464e8b112dd40b04379f5c5bc4876fda65a5
Kacper Hałuszczak <anakinud@o2.pl>
2018-07-19 23:18:51 +0200

    Added property existance check #48

--- TPA/Reflection/Execution/DataBinding.cs
+++ TPA/Reflection/Execution/DataBinding.cs
@@ -15,8 +15,11 @@ namespace TPA.Reflection.Execution
         private readonly object m_consumer;
         private readonly PropertyInfo m_consumerProperty;
 
-        public DataBinding(INotifyPropertyChanged dataSource, PropertyInfo sourceProperty, object consumer, PropertyInfo consumerProperty)
+        public DataBinding(INotifyPropertyChanged dataSource, string sourcePropertyName, object consumer, string consumerPropertyName)
         {
+            PropertyInfo sourceProperty = dataSource.GetType().GetProperty(sourcePropertyName) ?? throw new ArgumentException("No such Property in Data Source");
+            PropertyInfo consumerProperty = consumer.GetType().GetProperty(consumerPropertyName) ?? throw new ArgumentException("No such Property in Data Consumer");
+
             if (!consumerProperty.PropertyType.IsAssignableFrom(sourceProperty.PropertyType))
             {
                 throw new ArgumentException("Properties types don't match");
