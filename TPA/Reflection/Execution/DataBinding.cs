aa9f23a8360854761912c5024e53786dfe614c50
Kacper Hałuszczak <anakinud@o2.pl>
2018-07-19 23:03:31 +0200

    Proposition of data binding #48

--- TPA/Reflection/Execution/DataBinding.cs
+++ TPA/Reflection/Execution/DataBinding.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TPA.Reflection.Execution
 {
-  class DataBinding
+  internal class DataBinding
   {
+        private readonly PropertyInfo m_source;
+
+        private readonly object m_consumer;
+        private readonly PropertyInfo m_consumerProperty;
+
+        public DataBinding(INotifyPropertyChanged dataSource, PropertyInfo sourceProperty, object consumer, PropertyInfo consumerProperty)
+        {
+            if (!consumerProperty.PropertyType.IsAssignableFrom(sourceProperty.PropertyType))
+            {
+                throw new ArgumentException("Properties types don't match");
+            }
+
+            m_source = sourceProperty;
+
+            m_consumer = consumer;
+            m_consumerProperty = consumerProperty;
+
+            dataSource.PropertyChanged += OnPropertyChangedAction;
+        }
+
+        private void OnPropertyChangedAction(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == m_source.Name)
+            {
+                m_consumerProperty.SetValue(m_consumer, m_source.GetValue(sender));
+            }
+        }
   }
 }
