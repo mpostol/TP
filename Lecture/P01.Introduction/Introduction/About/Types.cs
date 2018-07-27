9700d4a5ffdaac506d8ea1f1b5e96cc16c790274
MPostol <mpostol@cas.eu>
2018-07-27 13:08:15 +0200

    Part 1. Introduction About the course #113
    - added placeholder
    - first example - must be improved.

---
+++ Lecture/P01.Introduction/Introduction/About/Types.cs
@@ -0,0 +1,24 @@
+﻿//____________________________________________________________________________
+//
+//  Copyright (C) 2018, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
+namespace TP.Introduction.About
+{
+  public static class Types
+  {
+
+    public static void TypeCompatibility()
+    {
+      long ArabicIntegerNumber = 4;
+      float ArabicFloatNumber = 4.0f;
+      //ArabicIntegerNumber = 4.0f;
+      ArabicFloatNumber = 4;
+      bool isEqual = 4 == 4.0f;
+      string RomanIntegerNumber = "IV";
+    }
+  }
+}
+
