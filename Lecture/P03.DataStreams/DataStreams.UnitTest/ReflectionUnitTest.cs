a36f36f65b27476600045a90cacc934f7bcf70bb
MPostol <mpostol@cas.eu>
2018-07-29 14:54:22 +0200

    Part 3. Data streams : Attributes #116
    - review existing code
    - add a placeholder to the project
    - add code example - consider merging with type compatibility
    - add unit tests

---
+++ Lecture/P03.DataStreams/DataStreams.UnitTest/ReflectionUnitTest.cs
@@ -0,0 +1,38 @@
+﻿//____________________________________________________________________________
+//
+//  Copyright (C) 2018, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TP.DataStreams.Reflection;
+
+namespace TP.DataStreams
+{
+  [TestClass]
+  public class ReflectionUnitTest
+  {
+
+    [TestMethod]
+    public void CustomAttributeTest()
+    {
+      CustomAttribute _attribute = new CustomAttribute() { Description = "Instance description" };
+      Assert.IsNotNull(_attribute);
+      Assert.AreEqual<string>("Instance description", _attribute.Description);
+    }
+    [TestMethod]
+    public void AttributedClassTest()
+    {
+      object _unknowObject = AttributedClass.GetObject();
+      Type _objectType = _unknowObject.GetType();
+      Object[] _attribute = _objectType.GetCustomAttributes(typeof(CustomAttribute), false);
+      Assert.AreEqual<int>(1,  _attribute.Length);
+      CustomAttribute _expectedAttribute = _attribute[0] as CustomAttribute;
+      Assert.IsNotNull(_expectedAttribute);
+      Assert.AreEqual<string>("Description of the class", _expectedAttribute.Description);
+    }
+
+  }
+}
---
+++ Lecture/P03.DataStreams/DataStreams/Reflection/AttributedClass.cs
@@ -0,0 +1,27 @@
+﻿//____________________________________________________________________________
+//
+//  Copyright (C) 2018, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
+using System;
+
+namespace TP.DataStreams.Reflection
+{
+
+  [CustomAttribute(Description = "Description of the class")]
+  public class AttributedClass
+  {
+    public static object GetObject()
+    {
+      return new AttributedClass();
+    }
+  }
+  [AttributeUsage(AttributeTargets.Class)]
+  public class CustomAttribute : Attribute
+  {
+    public string Description { get; set; }
+  }
+
+}
