e249d478e19ec1d69980aad0e3fae00beada6225
MPostol <mpostol@cas.eu>
2018-07-30 13:01:49 +0200

    TPA - Reflection method and property get/set call example #48
    - reviewed and refactored to meet the lecture requirements
    - UT :+1:

--- TPA/Reflection/Execution/DataBinding.cs
+++ TPA/Reflection/Execution/DataBinding.cs
@@ -1,51 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//____________________________________________________________________________
+//
+//  Copyright (C) 2018, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
+using System;
 using System.ComponentModel;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TPA.Reflection.Execution
 {
   internal class DataBinding
   {
-    private readonly PropertyInfo m_source;
-
-    private readonly object m_consumer;
-    private readonly PropertyInfo m_consumerProperty;
 
     public DataBinding(INotifyPropertyChanged dataSource, string sourcePropertyName, object consumer, string consumerPropertyName)
     {
-      PropertyInfo sourceProperty = dataSource.GetType().GetProperty(sourcePropertyName) ?? throw new ArgumentException("No such Property in Data Source");
-      PropertyInfo consumerProperty = consumer.GetType().GetProperty(consumerPropertyName) ?? throw new ArgumentException("No such Property in Data Consumer");
-      
-      if (!consumerProperty.PropertyType.IsAssignableFrom(sourceProperty.PropertyType))
-      {
+      PropertyInfo _sourceProperty = dataSource.GetType().GetProperty(sourcePropertyName) ?? throw new ArgumentException("No such Property in Data Source");
+      PropertyInfo _consumerProperty = consumer.GetType().GetProperty(consumerPropertyName) ?? throw new ArgumentException("No such Property in Data Consumer");
+      if (!_consumerProperty.PropertyType.IsAssignableFrom(_sourceProperty.PropertyType))
         throw new ArgumentException("Properties types don't match");
-      }
-      
-      m_source = sourceProperty;
-      
+      m_source = _sourceProperty;
       m_consumer = consumer;
-      m_consumerProperty = consumerProperty;
-      
+      m_consumerProperty = _consumerProperty;
       dataSource.PropertyChanged += OnPropertyChangedAction;
-      
       UpdateProperty(dataSource);
     }
 
+    private readonly PropertyInfo m_source;
+    private readonly object m_consumer;
+    private readonly PropertyInfo m_consumerProperty;
     private void OnPropertyChangedAction(object sender, PropertyChangedEventArgs args)
     {
       if (args.PropertyName == m_source.Name)
-      {
         UpdateProperty(sender);
-      }
     }
-
     private void UpdateProperty(object dataSource)
     {
       m_consumerProperty.SetValue(m_consumer, m_source.GetValue(dataSource));
     }
+
   }
 }
--- TPA/ReflectionUnitTest/Execution/DataSource.cs
+++ TPA/ReflectionUnitTest/Execution/DataSource.cs
@@ -1,33 +1,12 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿//____________________________________________________________________________
+//
+//  Copyright (C) 2018, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
 
 namespace TPA.Reflection.UnitTest.Execution
 {
-  internal class DataSource : INotifyPropertyChanged
-  {
-    private int m_intField = 0;
-    public int IntProperty {
-      get { return m_intField; }
-      set {
-        m_intField = value;
-        OnPropertyChanged("IntProperty");
-      }
-    }
-  
-    private string m_stringField = "";
-    public string StringProperty {
-      get { return m_stringField; }
-      set {
-        m_stringField = value;
-        OnPropertyChanged("StringProperty");
-      }
-    }
-  
-    public event PropertyChangedEventHandler PropertyChanged;
-  
-    private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-    {
-      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-  }
+
 }
