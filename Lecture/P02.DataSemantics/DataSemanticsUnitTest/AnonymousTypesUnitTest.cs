6ffbf2df24e1833afb8076ea6f417036ff1aacc1
mpostol <mpostol@cas.eu>
2018-08-24 12:32:50 +0200

    2. Data Semantics - tuple types example #127
    - working on the tuple examples
    - UT :+1:
    - It is done for now.

--- Lecture/P02.DataSemantics/DataSemanticsUnitTest/AnonymousTypesUnitTest.cs
+++ Lecture/P02.DataSemantics/DataSemanticsUnitTest/AnonymousTypesUnitTest.cs
@@ -83,69 +83,62 @@ namespace TP.DataSemantics
     [TestMethod]
     public void AnonymousArrayTest()
     {
-      var anonArray = new[] {
+      var _anonymousArray = new[] {
                               new { name = "apple", diam = 4 },
                               new { name = "grape", diam = 1 },
                               //new { diam = 2, name = "plum"  } 
                              };
-      Assert.AreEqual(new { name = "apple", diam = 4 }, anonArray[0]);
-      Assert.AreEqual(new { name = "grape", diam = 1 }, anonArray[1]);
+      Assert.AreEqual(new { name = "apple", diam = 4 }, _anonymousArray[0]);
+      Assert.AreEqual(new { name = "grape", diam = 1 }, _anonymousArray[1]);
     }
     [TestMethod]
-    public void ValueTupleTypeTest()
+    public void NoNewBehaviorValuesTypeTest()
     {
-      var _anonymousVariable1 = (108, "Hello" );
-      Assert.AreEqual(108, _anonymousVariable1.Item1);
-      Assert.AreEqual("Hello", _anonymousVariable1.Item2);
-      _anonymousVariable1.Item1 = 801;
-      _anonymousVariable1.Item2 = "";
-      //_anonymousVariable1 = ( 108.0, "Hello"); //Error CS0029  Cannot implicitly convert type 'double' to 'int' 
-      //_anonymousVariable1 = ("Hello", 108); //Error CS0029  Cannot implicitly convert type 'string' to 'int'; Error CS0029  Cannot implicitly convert type 'int' to 'string' 
-      //_anonymousVariable1 = null; //Error CS0037  Cannot convert null to '(int, string)' because it is a non - nullable value type
+      Tuple<int, string> _ = new Tuple<int, string>(108, "Hello");
+      ValueTuple<int, string> _valueTupleVariable = (108, "Hello" );
+      //ValueTuple<int, string> _ = ValueTuple.Create<int, string>(108, "Hello");
+      Assert.AreEqual(108, _valueTupleVariable.Item1);
+      Assert.AreEqual("Hello", _valueTupleVariable.Item2);
+      _valueTupleVariable.Item1 = 801;
+      _valueTupleVariable.Item2 = "";
+      //_valueTupleVariable = (108.0, "Hello"); //Error CS0029  Cannot implicitly convert type 'double' to 'int' 
+      //_valueTupleVariable = ("Hello", 108); //Error CS0029  Cannot implicitly convert type 'string' to 'int'; Error CS0029  Cannot implicitly convert type 'int' to 'string' 
+      //_valueTupleVariable = null; //Error CS0037  Cannot convert null to '(int, string)' because it is a non - nullable value type
     }
     [TestMethod]
-    public void TypedValueTupleTypeTest()
+    public void NamedFieldsValueTupleTypeTest()
     {
-      var _anonymousVariable1 = (Amount : 108, Message: "Hello");
-      Assert.AreEqual(108, _anonymousVariable1.Item1);
-      Assert.AreEqual("Hello", _anonymousVariable1.Item2);
-      _anonymousVariable1.Amount = 801;
-      _anonymousVariable1.Message = "";
+      //(int Amount, string Message) _ = (108, "Hello");
+      var _valueTupleVariable = (Amount: 108, Message: "Hello");
+      Assert.AreEqual(108, _valueTupleVariable.Item1);
+      Assert.AreEqual(108, _valueTupleVariable.Amount);
+      Assert.AreEqual("Hello", _valueTupleVariable.Item2);
+      Assert.AreEqual("Hello", _valueTupleVariable.Message);
+      _valueTupleVariable.Amount = 801;
+      _valueTupleVariable.Message = "";
     }
     [TestMethod]
-    public void NamedValueTupleTypeTest()
+    public void ValueTupleEqalityTest()
     {
-      (int Amount, string Message) _anonymousVariable1 = (108, "Hello");
-      Assert.AreEqual(108, _anonymousVariable1.Item1);
-      Assert.AreEqual(108, _anonymousVariable1.Amount);
-      Assert.AreEqual("Hello", _anonymousVariable1.Item2);
-      Assert.AreEqual("Hello", _anonymousVariable1.Message);
-      _anonymousVariable1.Amount = 801;
-      _anonymousVariable1.Message = "";
-    }
-    public void TupleValueTypesEqalityTest()
-    {
-      (int Amount, string Message) _anonymousVariable1 = (108, "Hello");
-      (int Amount, string Message) _anonymousVariable2 = (108, "Hello");
-      //Assert.IsTrue(_anonymousVariable1 == _anonymousVariable2);//Error CS8107  Feature 'tuple equality' is not available in C# 7.0. Please use language version 7.3 or greater.
-      Assert.AreEqual(_anonymousVariable1, _anonymousVariable2);
-      Assert.AreNotSame(_anonymousVariable1, _anonymousVariable2);
-      _anonymousVariable1 = _anonymousVariable2;
-      Assert.AreSame(_anonymousVariable1, _anonymousVariable2);
+      (int Amount, string Message) _valueTupleVariable1 = (108, "Hello");
+      (int Amount, string Message) _valueTupleVariable2 = (108, "Hello");
+      Assert.IsTrue(_valueTupleVariable1 == _valueTupleVariable2); //Error CS8107  Feature 'tuple equality' is not available in C# 7.0. Please use language version 7.3 or greater.
+      Assert.AreEqual(_valueTupleVariable1, _valueTupleVariable2);
+      _valueTupleVariable1 = _valueTupleVariable2;
     }
     [TestMethod]
-    public void TupleMethodTestMethod()
+    public void ValueTupleMethodTest()
     {
-      var _anonymousVariable1 = TupleClass.GetTupleValue();
-      Assert.AreEqual(108, _anonymousVariable1.Item1);
-      Assert.AreEqual("Hello", _anonymousVariable1.Item2);
-      _anonymousVariable1.Amount = 801;
-      _anonymousVariable1.Message = "";
+      var _valueTupleVariable = TupleClass.GetValueTupleValue();
+      Assert.AreEqual(108, _valueTupleVariable.Item1);
+      Assert.AreEqual("Hello", _valueTupleVariable.Item2);
+      _valueTupleVariable.Amount = 801;
+      _valueTupleVariable.Message = "";
     }
     [TestMethod]
-    public void MyTestMethod()
+    public void ValueTupleDeconstructionMethodTest()
     {
-      (int Amount, string Message) = TupleClass.GetTupleValue();
+      (int Amount, string Message) = TupleClass.GetValueTupleValue();
       Assert.AreEqual(108, Amount);
       Assert.AreEqual("Hello", Message);
       Amount = 801;
@@ -153,7 +146,7 @@ namespace TP.DataSemantics
     }
     private static class TupleClass
     {
-      internal static (int Amount, string Message) GetTupleValue()
+      internal static (int Amount, string Message) GetValueTupleValue()
       {
         return (108, "Hello");
       }
