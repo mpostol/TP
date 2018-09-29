ad6fe7004bf776204028883a45e48ed36a325890
mpostol <mpostol@cas.eu>
2018-09-29 16:08:07 +0200

    Part 1. Introduction - prepare issues #109 - added conversion of the roman numerals to integers - added unit tests - UT :1+:

---
+++ Lecture/P01.Introduction/Introduction.UnitTest/TypesConceptUnitTest.cs
@@ -0,0 +1,37 @@
+﻿#pragma warning disable CS0219 // Variable is assigned but its value is never used
+//___________________________________________________________________________________
+//
+//  Copyright (C) 2018, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
+//___________________________________________________________________________________
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TP.Introduction
+{
+
+  [TestClass]
+  public class TypesConceptUnitTest
+  {
+    [TestMethod]
+    public void TypesCompatibilityTest()
+    {
+      long ArabicIntegerNumber = 4;
+      float ArabicFloatNumber = 4.0f;
+      //ArabicIntegerNumber = 4.0f;
+      ArabicFloatNumber = 4;
+      bool isEqual = 4 == 4.0f;
+      string RomanIntegerNumber = "IV";
+    }
+    [TestMethod]
+    public void RomanToIntegerTest()
+    {
+      int _interger = About.Types.RomanToInteger("IV");
+      Assert.AreEqual(4, _interger);
+      _interger = About.Types.RomanToInteger("IVXX");
+      Assert.AreEqual(14, _interger);
+    }
+  }
+}
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
--- Lecture/P01.Introduction/Introduction/About/Types.cs
+++ Lecture/P01.Introduction/Introduction/About/Types.cs
@@ -5,20 +5,35 @@
 //  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
 //____________________________________________________________________________
 
+using System.Collections.Generic;
+
 namespace TP.Introduction.About
 {
   public static class Types
   {
 
-    public static void TypeCompatibility()
+    public static int RomanToInteger(string roman)
     {
-      long ArabicIntegerNumber = 4;
-      float ArabicFloatNumber = 4.0f;
-      //ArabicIntegerNumber = 4.0f;
-      ArabicFloatNumber = 4;
-      bool isEqual = 4 == 4.0f;
-      string RomanIntegerNumber = "IV";
+      int number = 0;
+      for (int i = 0; i < roman.Length; i++)
+      {
+        if (i + 1 < roman.Length && RomanMap[roman[i]] < RomanMap[roman[i + 1]])
+          number -= RomanMap[roman[i]];
+        else
+          number += RomanMap[roman[i]];
+      }
+      return number;
     }
+    private static readonly Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
   }
 }
 
