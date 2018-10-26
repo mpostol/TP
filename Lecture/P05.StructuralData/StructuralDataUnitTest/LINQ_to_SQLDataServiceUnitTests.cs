1b643b814b11770cebfb40e9bc1b20409b7670bc
mpostol <mpostol@cas.eu>
2018-10-26 23:16:17 +0200

    5.3. LINQ to SQL #121 - working copy - not all files has been committed
    - removed LinqToSqlApp.csproj project - they have been moved to UT project and must be rewritten.
    - UT :1+:

--- Lecture/P05.StructuralData/StructuralDataUnitTest/LINQ_to_SQLDataServiceUnitTests.cs
+++ Lecture/P05.StructuralData/StructuralDataUnitTest/LINQ_to_SQLDataServiceUnitTests.cs
@@ -122,6 +122,33 @@ namespace TP.StructuralDataUnitTest
     //  CollectionAssert.AreEqual(originalAge, finalAge);
     //  PrintPersons(youngPersons);
     //}
+    //TODO review; it is from LinqToSqlApp
+    //private static void Main(string[] args)
+    //{
+    //// Connection string defined in LinqToSqlApp project settings.
+    //string connectionString = global::LinqToSqlApp.Properties.Settings.Default.PersonDataConnectionString;
+    //using (PersonService service = new PersonService(connectionString))
+    //{
+    //  IEnumerable<Person> all = service.GetAllPersons();
+    //  Display("Everyone", all);
+    //  Console.WriteLine();
+    //  const string lastName = "Kowalski";
+    //  const int minAge = 25;
+    //  const int change = 11;
+    //  Display("With last name '" + lastName + "'", service.FilterPersonsByLastName(lastName));
+    //  Console.WriteLine();
+    //  Display("After finishing studies", service.FilterPersonsByMinAge(minAge));
+    //  Display("Forward " + change + " years", service.ChangeAgeThenFilterPersonsByMinAge(change, minAge));
+    //  Display("And back " + change + " years", service.ChangeAgeThenFilterPersonsByMinAge(-change, minAge));
+    //}
+    //Console.ReadLine();
+    //}
+    //private static void Display(string title, IEnumerable<Person> data)
+    //{
+    //  Console.WriteLine("*** {0} ***", title);
+    //  foreach (Person p in data)
+    //    Console.WriteLine("Person: {0} {1}, age {2}", p.FirstName, p.LastName, p.Age);
+    //}
 
     #region instrumentation
     // Connection string defined in LinqToSqlLibTests project settings.
