2ea4d5b75a60024da73f68b203081311548b6f9a
mpostol <mpostol@cas.eu>
2018-10-26 22:57:02 +0200

    5.3. LINQ to SQL #121 - working copy
    - removed LinqToSqlLib and LinqToSqlLibTests project - they have been consolidated and rewritten.
    - UT :1+:

--- Lecture/P05.StructuralData/LinqToSqlLib/dbo.Persons.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Persons] (
-    [PersonId]  INT          IDENTITY (1, 1) NOT NULL,
-    [FirstName] VARCHAR (50) NOT NULL,
-    [LastName]  VARCHAR (50) NOT NULL,
-    [Age]       INT          NOT NULL,
-    PRIMARY KEY CLUSTERED ([PersonId] ASC)
-);
