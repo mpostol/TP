59aaabe1c2c842b295ed4dcf114469b00c853a02
Przemek Fortuna <przemyslaw.fortuna@gmail.com>
2018-11-22 21:08:16 +0100

    Added a few kinds of types to example dll

---
+++ TPA/ApplicationArchitecture/Data/StaticClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPA.ApplicationArchitecture.Data
+{
+    public static class StaticClass
+    {
+        /*TODO:
+        + class 
+        + structure
+        - nested class
+        + static class
+        - enum
+        + inheritance
+        + abstact class
+        + interface
+        - delegate
+        + attribute
+        + property
+        + field
+        + circular reference
+        - generic class
+        */
+        public static int StaticField;
+        public static int StaticProperty { get; set; }
+        public static void StaticMethod1() { }
+        public static float StaticMethod2() { return 0f; }
+    }
+}
