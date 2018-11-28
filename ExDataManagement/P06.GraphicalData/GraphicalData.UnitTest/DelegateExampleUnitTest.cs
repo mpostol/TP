1f724593ee24224008e8d9829d918298af2700d1
mpostol <mpostol@cas.eu>
2018-11-28 17:38:07 +0100

    Add delegate/event example fixes #14
    - added event example

--- ExDataManagement/P06.GraphicalData/GraphicalData.UnitTest/DelegateExampleUnitTest.cs
+++ ExDataManagement/P06.GraphicalData/GraphicalData.UnitTest/DelegateExampleUnitTest.cs
@@ -39,6 +39,22 @@ namespace TP.GraphicalData
       _newInstance.PerformCalculationVar += new DelegateExample.PerformCalculation(DelegateExample.PerformSubtractMethod);
       Assert.AreEqual<int>(-1, _newInstance.PerformCalculationMethod(1, 2));
     }
+    [TestMethod]
+    public void PerformSumMethodCalledTest()
+    {
+      DelegateExample _newInstance = new DelegateExample();
+      int _Called = 0;
+      object _sender = null;
+      EventArgs _args = null;
+      _newInstance.PerformSumMethodCalled += (x, y) => { _Called++; _sender = x; _args = y; };
+      //_newInstance.PerformSumMethodCalled = (x, y) => { _Called++; _sender = x; _args = y; }; // The event 'DelegateExample.PerformSumMethodCalled' can only appear on the left hand side of += or -= (except when used from within the type 'DelegateExample')
+      //_newInstance.PerformSumMethodCalled(this, EventArgs.Empty); //The event 'DelegateExample.PerformSumMethodCalled' can only appear on the left hand side of += or -= (except when used from within the type 'DelegateExample')	
+      Assert.AreEqual<int>(0, _Called);
+      _newInstance.PerformSumMethod(1, 2);
+      Assert.AreEqual<int>(1, _Called);
+      Assert.AreSame(_newInstance, _sender);
+      Assert.AreSame(_args, EventArgs.Empty);
+    }
     private int PerformSumMethod(int x, int y)
     {
       checked
