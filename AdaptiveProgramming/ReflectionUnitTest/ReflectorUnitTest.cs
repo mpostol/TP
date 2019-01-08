c0bcd33ba71f5dd0580418a27902253865f86182
mpostol <mpostol@cas.eu>
2019-01-08 14:48:34 +0100

    Reflection - add Unit test to Reflector class #132
    - removed cloned text - work in progress.
    - :+1:

--- AdaptiveProgramming/ReflectionUnitTest/ReflectorUnitTest.cs
+++ AdaptiveProgramming/ReflectionUnitTest/ReflectorUnitTest.cs
@@ -7,6 +7,7 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -22,9 +23,11 @@ namespace TPA.Reflection.UnitTest
     public void ReflectorConstructorTest()
     {
       Assert.ThrowsException<ArgumentNullException>(() => new Reflector(string.Empty));
-      FileInfo _fileInfo = new FileInfo(ReflectorTestClass.m_TestAssemblyName);
+      FileInfo _fileInfo = new FileInfo(ReflectorTestClass.TestAssemblyName);
       Assert.IsTrue(_fileInfo.Exists);
-      Assert.IsNotNull(m_Reflector);
+      Assert.IsNotNull(ReflectorTestClass.Reflector);
+      Assert.AreEqual<int>(2, ReflectorTestClass.Reflector.Namespaces.Count);
+      Assert.IsNotNull(ReflectorTestClass.Reflector.MyNamespace);
     }
     [TestMethod]
     public void AssemblyNameTest()
@@ -43,23 +46,21 @@ namespace TPA.Reflection.UnitTest
     [TestMethod]
     public void AbstractClassTest()
     {
-      TypeMetadata abstractClass = m_Reflector.m_AssemblyModel.m_Namespaces.Single<NamespaceMetadata>(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
-          .m_Types.Single<TypeMetadata>(x => x.m_typeName == "AbstractClass");
+      TypeMetadata abstractClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single<TypeMetadata>(x => x.m_typeName == "AbstractClass");
       Assert.AreEqual(AbstractENum.Abstract, abstractClass.m_Modifiers.Item3);
       Assert.AreEqual(AbstractENum.Abstract, abstractClass.m_Methods.Single(x => x.m_Name == "AbstractMethod").m_Modifiers.Item2);
     }
     [TestMethod]
     public void ClassWithAttributesTest()
     {
-      TypeMetadata attributeClass = m_Reflector.m_AssemblyModel.m_Namespaces.Single<NamespaceMetadata>(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data").m_Types.Single(x => x.m_typeName == "ClassWithAttribute");
+      TypeMetadata attributeClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single(x => x.m_typeName == "ClassWithAttribute");
       Assert.AreEqual(1, attributeClass.m_Attributes.Count<CustomAttributeData>());
       //TypeMetaData lacks Fields info
     }
     [TestMethod]
     public void DerivedClassTest()
     {
-      TypeMetadata derivedClass = m_Reflector.m_AssemblyModel.m_Namespaces.Single(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
-          .m_Types.Single(x => x.m_typeName == "DerivedClass");
+      TypeMetadata derivedClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single(x => x.m_typeName == "DerivedClass");
       Assert.IsNotNull(derivedClass.m_BaseType);
     }
     [TestMethod]
@@ -72,9 +73,7 @@ namespace TPA.Reflection.UnitTest
     [TestMethod]
     public void GenericClassTest()
     {
-      TypeMetadata genericClass = m_Reflector.m_AssemblyModel.m_Namespaces
-          .Single<NamespaceMetadata>(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
-          .m_Types.Single<TypeMetadata>(x => x.m_typeName.Contains("GenericClass"));
+      TypeMetadata genericClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single<TypeMetadata>(x => x.m_typeName.Contains("GenericClass"));
       Assert.AreEqual(1, genericClass.m_GenericArguments.Count());
       Assert.AreEqual<string>("T", genericClass.m_GenericArguments.Single().m_typeName);
       Assert.AreEqual<string>("T", genericClass.m_Properties.Single<PropertyMetadata>(x => x.m_Name == "GenericProperty").m_TypeMetadata.m_typeName);
@@ -86,22 +85,16 @@ namespace TPA.Reflection.UnitTest
     [TestMethod]
     public void InterfaceTest()
     {
-      TypeMetadata interfaceClass = m_Reflector.m_AssemblyModel.m_Namespaces
-          .Single<NamespaceMetadata>(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
-          .m_Types.Single<TypeMetadata>(x => x.m_typeName == "IExample");
+      TypeMetadata interfaceClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single<TypeMetadata>(x => x.m_typeName == "IExample");
       Assert.AreEqual<TypeMetadata.TypeKind>(TypeMetadata.TypeKind.InterfaceType, interfaceClass.m_TypeKind);
-      Assert.AreEqual< AbstractENum>(AbstractENum.Abstract, interfaceClass.m_Modifiers.Item3);
+      Assert.AreEqual<AbstractENum>(AbstractENum.Abstract, interfaceClass.m_Modifiers.Item3);
       Assert.AreEqual<AbstractENum>(AbstractENum.Abstract, interfaceClass.m_Methods.Single<MethodMetadata>(x => x.m_Name == "MethodA").m_Modifiers.Item2);
     }
     [TestMethod]
     public void ImplementedInterfaceTest()
     {
-      TypeMetadata _interfaceClass = m_Reflector.m_AssemblyModel.m_Namespaces
-          .Single<NamespaceMetadata>(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
-          .m_Types.Single<TypeMetadata>(x => x.m_typeName == "IExample");
-      TypeMetadata implementedInterfaceClass = m_Reflector.m_AssemblyModel.m_Namespaces
-          .Single<NamespaceMetadata>(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
-          .m_Types.Single<TypeMetadata>(x => x.m_typeName == "ImplementationOfIExample");
+      TypeMetadata _interfaceClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single<TypeMetadata>(x => x.m_typeName == "IExample");
+      TypeMetadata implementedInterfaceClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single<TypeMetadata>(x => x.m_typeName == "ImplementationOfIExample");
       Assert.AreEqual<string>("IExample", implementedInterfaceClass.m_ImplementedInterfaces.Single().m_typeName);
       foreach (MethodMetadata method in _interfaceClass.m_Methods)
         Assert.IsNotNull(implementedInterfaceClass.m_Methods.SingleOrDefault(x => x.m_Name == method.m_Name));
@@ -115,18 +108,14 @@ namespace TPA.Reflection.UnitTest
     [TestMethod]
     public void NestedClassTest()
     {
-      TypeMetadata outerClass = m_Reflector.m_AssemblyModel.m_Namespaces
-          .Single<NamespaceMetadata>(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
-          .m_Types.Single<TypeMetadata>(x => x.m_typeName == "OuterClass");
+      TypeMetadata outerClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single<TypeMetadata>(x => x.m_typeName == "OuterClass");
       Assert.IsTrue(true);
       // TypeMetaData doesn't store information about private nested types (it does about public)
     }
     [TestMethod]
     public void StaticClassTest()
     {
-      TypeMetadata staticClass = m_Reflector.m_AssemblyModel.m_Namespaces
-          .Single(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
-          .m_Types.Single(x => x.m_typeName == "StaticClass");
+      TypeMetadata staticClass = ReflectorTestClass.Reflector.MyNamespace.m_Types.Single(x => x.m_typeName == "StaticClass");
       Assert.AreEqual(StaticEnum.Static, staticClass.m_Methods.Single(x => x.m_Name == "StaticMethod1").m_Modifiers.Item3);
       // no information about class/property being static
       // TypeMetaData lacks Fields info
@@ -134,22 +123,28 @@ namespace TPA.Reflection.UnitTest
     [TestMethod]
     public void StructureTest()
     {
-      TypeMetadata structure = m_Reflector.m_AssemblyModel.m_Namespaces
+      TypeMetadata structure = ReflectorTestClass.Reflector.m_AssemblyModel.m_Namespaces
           .Single<NamespaceMetadata>(x => x.m_NamespaceName == "TPA.ApplicationArchitecture.Data")
           .m_Types.Single<TypeMetadata>(x => x.m_typeName == "Structure");
-      Assert.AreEqual< TypeMetadata.TypeKind>(TypeMetadata.TypeKind.StructType, structure.m_TypeKind);
+      Assert.AreEqual<TypeMetadata.TypeKind>(TypeMetadata.TypeKind.StructType, structure.m_TypeKind);
     }
-
-    private Reflector m_Reflector => ReflectorTestClass.Reflector;
-    private class ReflectorTestClass: Reflector
+    private class ReflectorTestClass : Reflector
     {
       internal static ReflectorTestClass Reflector => m_Reflector.Value;
-      public ReflectorTestClass(): base(m_TestAssemblyName)
+      internal Dictionary<string, NamespaceMetadata> Namespaces;
+      internal NamespaceMetadata MyNamespace { get; private set; }
+      internal ReflectorTestClass() : base(TestAssemblyName)
       {
-
+        Namespaces = this.m_AssemblyModel.m_Namespaces.ToDictionary<NamespaceMetadata, string>(x => x.m_NamespaceName);
+        MyNamespace = Namespaces.ContainsKey(m_NamespaceName) ? Namespaces["TPA.ApplicationArchitecture.Data"] : null;
       }
-      internal const string m_TestAssemblyName = @"Instrumentation\TPA.ApplicationArchitecture.dll";
+      internal const string TestAssemblyName = @"Instrumentation\TPA.ApplicationArchitecture.dll";
+
+      #region private
+      private const string m_NamespaceName = "TPA.ApplicationArchitecture.Data";
       private static Lazy<ReflectorTestClass> m_Reflector = new Lazy<ReflectorTestClass>(() => new ReflectorTestClass());
+      #endregion
+
     }
   }
 }
