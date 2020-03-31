8e189443c7dc6e5dfe39746832eafffef80af1c4
mpostol <mpostol@cas.eu>
2020-03-31 22:28:01 +0200

    Add asynchronous programming example #174
    - Added example of the critical section

---
+++ DistributedProgramming/AsynchronousProgramming/AsynchronousLibrary/CriticalSectionExample.cs
@@ -0,0 +1,106 @@
+﻿//____________________________________________________________________________
+//
+//  Copyright (C) 2020, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPD.AsynchronousProgramming
+{
+  public class CriticalSectionExample
+  {
+    #region public API
+
+    public void StartThreads(bool useMonitor)
+    {
+      Thread[] _Threads = new Thread[2];
+      for (int i = 0; i < _Threads.Length; i++)
+        if (useMonitor)
+          _Threads[i] = new Thread(ThreadFuncWithMonitor);
+        else
+          _Threads[i] = new Thread(ThreadFunc);
+      foreach (Thread _thread in _Threads)
+        _thread.Start();
+      foreach (Thread _thread in _Threads)
+        _thread.Join();
+    }
+
+    public void StartThreadsUsingThreadPool(bool useMonitor)
+    {
+      for (int i = 0; i < 2; i++)
+        if (useMonitor)
+          ThreadPool.QueueUserWorkItem(ThreadFuncWithMonitor);
+        else
+          ThreadPool.QueueUserWorkItem(ThreadFunc);
+      //wait for threads
+      //TODO must be improved it could cause race condition
+      Thread.Sleep(1000);
+    }
+
+    public void StartThreadsUsingTask(bool useMonitor)
+    {
+      List<Task> _tasksInProgress = new List<Task>();
+      for (int i = 0; i < 2; i++)
+        if (useMonitor)
+          _tasksInProgress.Add(Task.Run(() => ThreadFuncWithMonitor(null)));
+        else
+          _tasksInProgress.Add(Task.Run(() => ThreadFunc(null)));
+      //wait for threads
+      Task.WaitAll(_tasksInProgress.ToArray());
+    }
+
+    #endregion public API
+
+    #region private
+
+    private readonly object m_SyncObject = new object();
+    private int m_IntegerA = 0;
+    private int m_IntegerB = 0;
+    private Random m_Random = new Random();
+    private bool m_IsConsistent = true;
+
+    private void ThreadFunc(object parameter)
+    {
+      DataProcessingSimulator();
+    }
+
+    private void ThreadFuncWithMonitor(object parameter)
+    {
+      lock (m_SyncObject)
+        DataProcessingSimulator();
+    }
+
+    private void DataProcessingSimulator()
+    {
+      for (int i = 0; i < 1000000; i++)
+      {
+        int _value = m_Random.Next(0, 10000);
+        m_IntegerA = _value;
+        m_IntegerB = -_value;
+        Thread.Sleep(0);
+        m_IsConsistent &= m_IntegerA + m_IntegerB == 0;
+      }
+    }
+
+    #endregion private
+
+    #region UT Instrumentation
+
+    /// <summary>
+    /// Gets a value indicating whether this instance is consistent.
+    /// </summary>
+    /// <remarks>Always shall return <c>true</c>.</remarks>
+    [System.Diagnostics.Conditional("DEBUG")]
+    public void GetConsistent(Action<bool> callback)
+    {
+      callback(m_IsConsistent);
+    }
+
+    #endregion UT Instrumentation
+  }
+}
---
+++ DistributedProgramming/AsynchronousProgramming/AsynchronousUnitTest/CriticalSectionExampleUnitTest.cs
@@ -0,0 +1,48 @@
+﻿//___________________________________________________________________________________
+//
+//  Copyright (C) 2020, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
+//___________________________________________________________________________________
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TPD.AsynchronousProgramming
+{
+  [TestClass]
+  public class CriticalSectionExampleUnitTest
+  {
+    [TestMethod]
+    public void StartThreadsMonitoredTest()
+    {
+      CriticalSectionExample _newSection = new CriticalSectionExample();
+      _newSection.StartThreads(true);
+      _newSection.GetConsistent(x => Assert.IsTrue(x));
+      _newSection = new CriticalSectionExample();
+      _newSection.StartThreads(false);
+      _newSection.GetConsistent(x => Assert.IsFalse(x));
+    }
+
+    [TestMethod]
+    public void StartThreadsUsingThreadPoolMonitoredTest()
+    {
+      CriticalSectionExample _newSection = new CriticalSectionExample();
+      _newSection.StartThreadsUsingThreadPool(true);
+      _newSection.GetConsistent(x => Assert.IsTrue(x));
+      _newSection = new CriticalSectionExample();
+      _newSection.StartThreadsUsingThreadPool(false);
+      _newSection.GetConsistent(x => Assert.IsFalse(x));
+    }
+
+    [TestMethod]
+    public void StartThreadsUsingTaskMonitoredTest()
+    {
+      CriticalSectionExample _newSection = new CriticalSectionExample();
+      _newSection.StartThreadsUsingTask(true);
+      _newSection.GetConsistent(x => Assert.IsTrue(x));
+      _newSection = new CriticalSectionExample();
+      _newSection.StartThreadsUsingTask(false);
+      _newSection.GetConsistent(x => Assert.IsFalse(x));
+    }
+  }
+}
