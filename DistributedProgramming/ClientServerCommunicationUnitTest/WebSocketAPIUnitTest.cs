507f534fd29e0af3c294c8c3a60e3754f9c96f92
Mariusz <mpostol@cas.eu>
2021-08-12 22:58:36 +0200

    WebSockets example #170
    - create UT - placeholder for further development
    - add anchor for the placeholder

---
+++ DistributedProgramming/ClientServerCommunicationUnitTest/WebSocketAPIUnitTest.cs
@@ -0,0 +1,28 @@
+﻿//____________________________________________________________________________
+//
+//  Copyright 2021 Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
+//____________________________________________________________________________
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TPD.Communication.ClientServerCommunication
+{
+  [TestClass]
+  public class WebSocketAPIUnitTest
+  {
+    [TestMethod]
+    public void WebSocketUsageTestMethod()
+    {
+      //TODO WebSockets example #170
+      Assert.Inconclusive("WebSockets example #170");
+      //create server
+      //connect client
+      //send testing data from client to the server (use serialization if possible)
+      //test correctness of the data
+      //respond from the server
+      //test correctness of the response
+    }
+  }
+}
