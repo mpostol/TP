25b3ffad76a808f4d6068f2e9a3488ce32a16b62
Mariusz <mpostol@cas.eu>
2021-09-08 17:07:49 +0200

    WebSockets example #170
    - fixes #170
    - UT :+1:

--- DistributedProgramming/ClientServerCommunicationUnitTest/WebSocketAPIUnitTest.cs
+++ DistributedProgramming/ClientServerCommunicationUnitTest/WebSocketAPIUnitTest.cs
@@ -1,85 +1,68 @@
-﻿//____________________________________________________________________________
+﻿//__________________________________________________________________________________________
 //
 //  Copyright 2021 Mariusz Postol LODZ POLAND.
 //
-//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
-//____________________________________________________________________________
+//  To be in touch join the community by pressing the `Watch` button and to get started 
+//  comment using the discussion panel at
+//  https://github.com/mpostol/TP/discussions/182
+//  with an introduction of yourself and tell us about what you do with this community.
+//__________________________________________________________________________________________
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using TPD.Communication.ClientServerCommunication.WebSocketAPI;
 
 namespace TPD.Communication.ClientServerCommunication
 {
-    [TestClass]
-    public class WebSocketAPIUnitTest
+  [TestClass]
+  public class WebSocketAPIUnitTest
+  {
+    [TestMethod]
+    public async Task WebSocketUsageTestMethod()
     {
-        private WebSocketConnection _wserver = null;
-        private WebSocketConnection _wclient = null;
-        private int _delay = 10;
+      WebSocketConnection _wserver = null;
+      WebSocketConnection _wclient = null;
+      const int _delay = 10;
 
-        //[TestCleanup]
-        //public async Task Cleanup()
-        //{
-        //    await _wclient?.DisconnectAsync();
-        //    await _wserver?.DisconnectAsync();
-        //}
+      //create server
+      Uri uri = new Uri("ws://localhost:6966");
+      List<string> logOutput = new List<string>();
+      Task server = Task.Run(async () => await WebSocketServer.Server(uri.Port,
+          _ws =>
+          {
+            _wserver = _ws; _wserver.onMessage = (data) =>
+                  {
+                    logOutput.Add($"Received message from client: { data}");
+                  };
+          }));
+      await Task.Delay(_delay); //To make sure that the server is listening for messages
+      //connect client
+      _wclient = await WebSocketClient.Connect(uri, message => logOutput.Add(message));
 
-        [TestMethod]
-        public async Task WebSocketUsageTestMethod()
-        {
-            //TODO WebSockets example #170
+      Assert.IsNotNull(_wserver);
+      Assert.IsNotNull(_wclient);
 
-            //create server
-            //Assert.Inconclusive("WebSockets example #170");
-            Uri uri = new Uri("ws://localhost:6966");
-            List<string> logOutput = new List<string>();
-            Action<string> log = (message) => logOutput.Add(message);
-            Task server = Task.Run(async () => await WebSocketServer.Server(uri.Port,
-                _ws =>
-                {
-                    _wserver = _ws; _wserver.onMessage = (data) =>
-                    {
-                        log($"Received message from client: { data}");
-                    };
-                }));
-            await Task.Delay(_delay);
+      //send testing data from client to the server (use serialization if possible)
+      Task clientSendTask = _wclient.SendAsync("test");
+      Assert.IsTrue(clientSendTask.Wait(new TimeSpan(0, 0, 1))); //To make sure that the send operation has been finished successfully
+      await Task.Delay(_delay); //To make sure that the message has been received by the server
 
+      //test correctness of the data
+      Assert.AreEqual($"Received message from client: test", logOutput[1]);
 
-            //connect client
-
-            _wclient = await WebSocketClient.Connect(uri, log);
-
-
-            Assert.IsNotNull(_wserver);
-            Assert.IsNotNull(_wclient);
-
-            //send testing data from client to the server (use serialization if possible)
-
-
-            await _wclient.SendAsync("test");
-            await Task.Delay(_delay);
-
-
-            //test correctness of the data
-            Assert.AreEqual($"Received message from client: test", logOutput[1]);
-
-            //respond from the server
-            _wclient.onMessage = (data) =>
-            {
-                log($"Received message from server: { data}");
-            };
-            await _wserver.SendAsync("test 2");
-            await Task.Delay(_delay);
-
-            //test correctness of the response
-            Assert.AreEqual($"Received message from server: test 2", logOutput[2]);
-
-            await _wclient?.DisconnectAsync();
-            await _wserver?.DisconnectAsync();
-        }
+      //respond from the server
+      _wclient.onMessage = (data) =>
+      {
+        logOutput.Add($"Received message from server: { data}");
+      };
+      Task serverSendTask =_wserver.SendAsync("test 2");
+      Assert.IsTrue(serverSendTask.Wait(new TimeSpan(0, 0, 1))); //To make sure that the send operation has been finished successfully
+      await Task.Delay(_delay); //To make sure that the message has been received by the server
+      Assert.AreEqual($"Received message from server: test 2", logOutput[2]); //test correctness of the response
+      await _wclient?.DisconnectAsync();
+      await _wserver?.DisconnectAsync();
     }
+  }
 }
