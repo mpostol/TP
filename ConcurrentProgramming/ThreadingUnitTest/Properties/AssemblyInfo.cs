35cf47726c5f46ea8d863ef6786b72fda4df75f0
MPostol <mpostol@cas.eu>
2022-01-28 22:44:29 +0100

    New solution for the Programming in Practice - Concurrent Programming #206
    - fixes #206

---
+++ ConcurrentProgramming/ThreadingUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+//__________________________________________________________________________________________
+//
+//  Copyright 2022 Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and to get started 
+//  comment using the discussion panel at
+//  https://github.com/mpostol/TP/discussions/182
+//  with an introduction of yourself and tell us about what you do with this community.
+//__________________________________________________________________________________________
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ThreadingUnitTest")]
+[assembly: AssemblyDescription("Threading Unit Test")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mariusz Postol")]
+[assembly: AssemblyProduct("Programming in Practice")]
+[assembly: AssemblyCopyright("Copyright 2022 Mariusz Postol")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("eea36658-fec8-44e1-bc27-5873a1e6f4e7")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
