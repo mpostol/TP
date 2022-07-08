0bd68f73d0a8905cfc6d4fc7a6a931c711de5ffc
MPostol <mpostol@cas.eu>
2022-07-08 16:24:51 +0200

    Improper Handling of Exceptional Conditions in Newtonsoft.Json #232
    - AdaptiveProgramming done

--- AdaptiveProgramming/TPA.Configuration/Properties/Settings.Designer.cs
+++ AdaptiveProgramming/TPA.Configuration/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TPA.Configuration.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -38,18 +38,6 @@ namespace TPA.Configuration.Properties {
         public global::System.Drawing.Color SettingColor {
             get {
                 return ((global::System.Drawing.Color)(this["SettingColor"]));
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Setting1 {
-            get {
-                return ((string)(this["Setting1"]));
-            }
-            set {
-                this["Setting1"] = value;
             }
         }
     }
