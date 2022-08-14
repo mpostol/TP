36e3b3111a0db51d60a5f9f055d9cef7b9192630
MPostol <mpostol@cas.eu>
2022-08-14 22:49:00 +0200

    CI Inter Layers Communication - Add Initial Stuff #239
    - Added CallBackTest

---
+++ InformationComputation/LayersCommunicationTest/CallBackTest.cs
@@ -0,0 +1,31 @@
+﻿//__________________________________________________________________________________________
+//
+//  Copyright 2022 Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and to get started
+//  comment using the discussion panel at
+//  https://github.com/mpostol/TP/discussions/182
+//  with an introduction of yourself and tell us about what you do with this community.
+//__________________________________________________________________________________________
+
+using TP.InformationComputation.LayersCommunication.Instrumentation;
+using TP.InformationComputation.LayersCommunication.Logic;
+
+namespace TP.InformationComputation.LayersCommunication
+{
+  [TestClass]
+  public class CallBackTest
+  {
+    [TestMethod]
+    public void CallBackTestMethod()
+    {
+      InMemoryTraceSource inMemoryTraceSource= new InMemoryTraceSource();
+      ICallBack callBackBased = LogicAbstraction.NewICallBack();
+      callBackBased.Alpha(inMemoryTraceSource.TraceData);
+      callBackBased.Bravo(inMemoryTraceSource.TraceData);
+      callBackBased.Charlie(inMemoryTraceSource.TraceData);
+      callBackBased.Delta(inMemoryTraceSource.TraceData);
+      inMemoryTraceSource.CheckConsistency();
+    }
+  }
+}
