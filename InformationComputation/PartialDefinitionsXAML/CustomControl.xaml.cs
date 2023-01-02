4a2cf74e85d1220c9ba79d5dbc708f2ee29a9a1b
MPostol <mpostol@cas.eu>
2023-01-02 23:19:08 +0100

    IC Code harmonization with the Partial Definitions #298
    - added CustomControl (xaml example)

---
+++ InformationComputation/PartialDefinitionsXAML/CustomControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿//__________________________________________________________________________________________
+//
+//  Copyright 2023 Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and to get started
+//  comment using the discussion panel at
+//  https://github.com/mpostol/TP/discussions/182
+//  with an introduction of yourself and tell us about what you do with this community.
+//__________________________________________________________________________________________
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TP.InformationComputation.PartialDefinitions
+{
+  /// <summary>
+  /// Interaction logic for UserControl.xaml
+  /// </summary>
+  public partial class CustomControl : UserControl
+  {
+    public CustomControl()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      //Do nothing
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+      //Do nothing
+    }
+  }
+}
