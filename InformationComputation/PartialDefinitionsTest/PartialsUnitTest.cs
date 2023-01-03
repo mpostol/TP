4d40c1db7f51215c3fda377bff9a8ba9b3e93481
MPostol <mpostol@cas.eu>
2023-01-03 16:00:07 +0100

    IC Code harmonization with the Partial Definitions #298
    - refactoring the examples
    - UT :+1:

--- InformationComputation/PartialDefinitionsTest/PartialsUnitTest.cs
+++ InformationComputation/PartialDefinitionsTest/PartialsUnitTest.cs
@@ -1,12 +1,15 @@
 ﻿//__________________________________________________________________________________________
 //
-//  Copyright 2022 Mariusz Postol LODZ POLAND.
+//  Copyright 2023 Mariusz Postol LODZ POLAND.
 //
 //  To be in touch join the community by pressing the `Watch` button and to get started
 //  comment using the discussion panel at
 //  https://github.com/mpostol/TP/discussions/182
 //  with an introduction of yourself and tell us about what you do with this community.
 //__________________________________________________________________________________________
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace TP.InformationComputation.PartialDefinitions
 {
