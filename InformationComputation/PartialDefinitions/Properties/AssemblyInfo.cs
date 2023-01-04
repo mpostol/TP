40cee146ca5438f80315a64d1daac842f42f5cf7
MPostol <mpostol@cas.eu>
2023-01-04 10:18:10 +0100

    IC Code harmonization with the Partial Definitions #298
    - updated assemblies inf for selected projects
    - UT :+1:

--- InformationComputation/PartialDefinitions/Properties/AssemblyInfo.cs
+++ InformationComputation/PartialDefinitions/Properties/AssemblyInfo.cs
@@ -1,51 +1,25 @@
-﻿using System.Reflection;
+﻿//__________________________________________________________________________________________
+//
+//  Copyright 2023 Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and to get started
+//  comment using the discussion panel at
+//  https://github.com/mpostol/TP/discussions/182
+//  with an introduction of yourself and tell us about what you do with this community.
+//__________________________________________________________________________________________
+
+using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("WpfCustomControlLibrary1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Partial Definitions")]
+[assembly: AssemblyDescription("examples related to partial type definitions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("WpfCustomControlLibrary1")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("MPostol")]
+[assembly: AssemblyProduct("Programming in Practice - Information Computation")]
+[assembly: AssemblyCopyright("Copyright 2023 Mariusz Postol")]
+[assembly: AssemblyTrademark("Programming in Practice")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-//In order to begin building localizable applications, set
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
-)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("C1AFB765-88D4-4479-937C-608416ABA3FE")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
