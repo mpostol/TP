f27eed5e8ba442ffa619a9b26167282eeb7c1ac3
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-04-05 13:10:07 +0200

    ExDataManagement copyright update in P01 project

--- ExDataManagement/P01.Introduction/DependencyInjection.Application/Program.cs
+++ ExDataManagement/P01.Introduction/DependencyInjection.Application/Program.cs
@@ -1,9 +1,13 @@
-﻿//____________________________________________________________________________
+﻿//____________________________________________________________________________________________________________________________________
 //
-//  Copyright (C) 2020, Mariusz Postol LODZ POLAND.
+//  Copyright 2020 Mariusz Postol LODZ POLAND.
 //
-//  To be in touch join the community at GITTER: https://gitter.im/mpostol/TP
-//____________________________________________________________________________
+//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
+//
+//  https://github.com/mpostol/TP/discussions/182
+//
+//  by introducing yourself and telling us what you do with this community.
+//_____________________________________________________________________________________________________________________________________
 
 using System;
 using TP.Introduction;
