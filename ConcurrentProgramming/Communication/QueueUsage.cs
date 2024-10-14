af0d4f08c1aa0f7a5995a07200d7adfd945b6e2c
MPostol <mariuszpostol@gmail.com>
2024-10-14 20:45:55 +0200

    CP Project Structure #408
    - backup copy - work in progress

---
+++ ConcurrentProgramming/Communication/QueueUsage.cs
@@ -0,0 +1,17 @@
+﻿
+//____________________________________________________________________________________________________________________________________
+//
+//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
+//
+//  https://github.com/mpostol/TP/discussions/182
+//
+//_____________________________________________________________________________________________________________________________________
+
+namespace TP.ConcurrentProgramming.Communication
+{
+  internal class QueueUsage
+  {
+  }
+}
