f08346640b53c5297031a368f823b0c44fe9edc4
MPostol <mariuszpostol@gmail.com>
2024-11-19 13:20:50 +0100

    CriticalSectionExample - add example to ConcurrentProgramming #218
    - moved threads creation from the CriticalSection to Unit CriticalSectionUnitTest - still needs improvements
    - UT :+1:

--- ConcurrentProgramming/CommonDataConsistency/CriticalSection.cs
+++ ConcurrentProgramming/CommonDataConsistency/CriticalSection.cs
@@ -1,5 +1,4 @@
-﻿
-//____________________________________________________________________________________________________________________________________
+﻿//____________________________________________________________________________________________________________________________________
 //
 //  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
 //
@@ -11,171 +10,117 @@
 
 namespace TP.ConcurrentProgramming.CommonDataConsistency
 {
-    public class CriticalSection
+  public class CriticalSection
+  {
+    #region Monitor methods
+
+    public int LockedNumber; // variable used to demonstrate how monitors works
+
+    public void NoMonitorMethod(object state)
     {
-        #region To be moved to UT
-
-        public void StartThreads(bool useMonitor)
-        {
-            Thread[] _Threads = new Thread[2];
-            for (int i = 0; i < _Threads.Length; i++)
-                if (useMonitor)
-                    _Threads[i] = new Thread(ThreadFuncWithMonitor);
-                else
-                    _Threads[i] = new Thread(ThreadFunc);
-            foreach (Thread _thread in _Threads)
-                _thread.Start();
-            foreach (Thread _thread in _Threads)
-                _thread.Join();
-        }
-
-        public void StartThreadsUsingThreadPool(bool useMonitor)
-        {
-            for (int i = 0; i < 2; i++)
-                if (useMonitor)
-                    ThreadPool.QueueUserWorkItem(ThreadFuncWithMonitor);
-                else
-                    ThreadPool.QueueUserWorkItem(ThreadFunc);
-            //wait for threads
-            //TODO must be improved it could cause race condition
-            Thread.Sleep(1000);
-        }
-
-        public void StartThreadsUsingTask(bool useMonitor)
-        {
-            List<Task> _tasksInProgress = new List<Task>();
-            for (int i = 0; i < 2; i++)
-                if (useMonitor)
-                    Task.Run(() => ThreadFuncWithMonitor(useMonitor));
-                else
-                    Task.Run(() => ThreadFunc(useMonitor));
-            //wait for threads
-            //TODO _tasksInProgress is always empty.
-            Task.WaitAll(_tasksInProgress.ToArray());
-        }
-
-        #endregion To be moved to UT
-
-        #region Monitor methods
-
-        public int LockedNumber; // variable used to demonstrate how monitors works
-
-        public void NoMonitorMethod(object state)
-        {
-            for (int i = 0; i < 1000000; ++i)
-                ++LockedNumber;
-        }
-
-        public void MonitorMethod(object state)
-        {
-            bool lockWasTaken = false;
-            try
-            {
-                Monitor.Enter(this, ref lockWasTaken);
-                for (int i = 0; i < 1000000; ++i)
-                    ++LockedNumber;
-            }
-            finally
-            {
-                if (lockWasTaken)
-                    Monitor.Exit(this);
-            }
-        }
-
-        public void LockMethod(object state)
-        {
-            lock (this)
-            {
-                for (int i = 0; i < 1000000; ++i)
-                    ++LockedNumber;
-            }
-        }
-
-        public void MonitorMethodWithTimeout(object state)
-        {
-            bool[] _parametersObjects = state as bool[]; // a flag used for testing
-            bool _lockWasTaken = false;
-            const int timeout = 1000; // 1 second
-            try
-            {
-                Monitor.TryEnter(this, timeout, ref _lockWasTaken);
-                if (_lockWasTaken)
-                    Thread.Sleep(2000); // 2 seconds
-            }
-            finally
-            {
-                if (_lockWasTaken)
-                    Monitor.Exit(this);
-                else
-                    _parametersObjects[0] = true;
-            }
-        }
-
-        // IMPORTANT: explaining the difference between ready queue and waiting queue
-        public void WaitMethod(object state)
-        {
-            lock (this)
-            {
-                for (int i = 0; i < 1000000; ++i)
-                    ++LockedNumber;
-                System.Threading.Monitor.Wait(this);
-                for (int i = 0; i < 2000000; ++i)
-                    --LockedNumber;
-            }
-        }
-
-        public void PulseMethod(object state)
-        {
-            lock (this)
-            {
-                for (int i = 0; i < 1000000; ++i)
-                    ++LockedNumber;
-                System.Threading.Monitor.Pulse(this);
-            }
-        }
-
-        #endregion Monitor methods
-
-        #region private
-
-        private readonly object m_SyncObject = new object();
-        private int m_IntegerA = 0;
-        private int m_IntegerB = 0;
-        private Random m_Random = new Random();
-
-        private void ThreadFunc(object parameter)
-        {
-            DataProcessingSimulator();
-        }
-
-        private void ThreadFuncWithMonitor(object parameter)
-        {
-            lock (m_SyncObject)
-                DataProcessingSimulator();
-        }
-
-        private void DataProcessingSimulator()
-        {
-            for (int i = 0; i < 1000000; i++)
-            {
-                int _value = m_Random.Next(0, 10000);
-                m_IntegerA = _value;
-                m_IntegerB = -_value;
-                IsConsistent &= m_IntegerA + m_IntegerB == 0;
-            }
-        }
-
-        #endregion private
-
-        #region UT Instrumentation
-
-        /// <summary>
-        /// Gets a value indicating whether this instance is consistent.
-        /// </summary>
-        /// <remarks>Always must be true.</remarks>
-        /// <value><c>true</c> if this instance is consistent; otherwise, <c>false</c>.</value>
-        public bool IsConsistent { get; private set; } = true;
-
-        #endregion UT Instrumentation
+      DataProcessingSimulator();
     }
+
+    public void MonitorMethod(object state)
+    {
+      bool lockWasTaken = false;
+      try
+      {
+        Monitor.Enter(this, ref lockWasTaken);
+        if (!lockWasTaken)
+          throw new ArgumentException();
+        DataProcessingSimulator();
+      }
+      finally
+      {
+        if (lockWasTaken)
+          Monitor.Exit(this);
+      }
+    }
+
+    public void LockMethod(object state)
+    {
+      lock (this)
+      {
+        for (int i = 0; i < 1000000; ++i)
+          ++LockedNumber;
+      }
+    }
+
+    public void MonitorMethodWithTimeout(object state)
+    {
+      bool[] _parametersObjects = state as bool[]; // a flag used for testing
+      bool _lockWasTaken = false;
+      const int timeout = 1000; // 1 second
+      try
+      {
+        Monitor.TryEnter(this, timeout, ref _lockWasTaken);
+        if (_lockWasTaken)
+          Thread.Sleep(2000); // 2 seconds
+      }
+      finally
+      {
+        if (_lockWasTaken)
+          Monitor.Exit(this);
+        else
+          _parametersObjects[0] = true;
+      }
+    }
+
+    // IMPORTANT: explaining the difference between ready queue and waiting queue
+    public void WaitMethod(object state)
+    {
+      lock (this)
+      {
+        for (int i = 0; i < 1000000; ++i)
+          ++LockedNumber;
+        System.Threading.Monitor.Wait(this);
+        for (int i = 0; i < 2000000; ++i)
+          --LockedNumber;
+      }
+    }
+
+    public void PulseMethod(object state)
+    {
+      lock (this)
+      {
+        for (int i = 0; i < 1000000; ++i)
+          ++LockedNumber;
+        System.Threading.Monitor.Pulse(this);
+      }
+    }
+
+    #endregion Monitor methods
+
+    #region private
+
+    private readonly object m_SyncObject = new object();
+    private int m_IntegerA = 0;
+    private int m_IntegerB = 0;
+    private Random m_Random = new Random();
+
+    private void DataProcessingSimulator()
+    {
+      for (int i = 0; i < 1000000; i++)
+      {
+        int _value = m_Random.Next(0, 10000);
+        m_IntegerA = _value;
+        m_IntegerB = -_value;
+        IsConsistent &= m_IntegerA + m_IntegerB == 0;
+      }
+    }
+
+    #endregion private
+
+    #region UT Instrumentation
+
+    /// <summary>
+    /// Gets a value indicating whether this instance is consistent.
+    /// </summary>
+    /// <remarks>Always must be true.</remarks>
+    /// <value><c>true</c> if this instance is consistent; otherwise, <c>false</c>.</value>
+    public bool IsConsistent { get; private set; } = true;
+
+    #endregion UT Instrumentation
+  }
 }
--- ConcurrentProgramming/CommonDataConsistencyUnitTest/CriticalSectionUnitTest.cs
+++ ConcurrentProgramming/CommonDataConsistencyUnitTest/CriticalSectionUnitTest.cs
@@ -1,109 +1,146 @@
 //____________________________________________________________________________________________________________________________________
 //
-//  Copyright (C) 2023, Mariusz Postol LODZ POLAND.
+//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
 //
 //  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
 //
 //  https://github.com/mpostol/TP/discussions/182
 //
-//  by introducing yourself and telling us what you do with this community.
 //_____________________________________________________________________________________________________________________________________
-
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Threading;
-using TP.ConcurrentProgramming.CommonDataConsistency;
 
 namespace TP.ConcurrentProgramming.CommonDataConsistency.UnitTest
 {
-    [TestClass]
-    public class CriticalSectionUnitTest
+  [TestClass]
+  public class CriticalSectionUnitTest
+  {
+    [TestMethod]
+    public void CheckWhetherThreadsAreNotSynchronized()
     {
-        [TestMethod]
-        public void CheckWhetherThreadsAreNotSynchronized()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            m_ThreadsExample.StartThreads(false);
-            Assert.IsFalse(m_ThreadsExample.IsConsistent);
-        }
-
-        [TestMethod]
-        public void CheckWhetherThreadsAreSynchronized()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            m_ThreadsExample.StartThreads(true);
-            Assert.IsTrue(m_ThreadsExample.IsConsistent);
-        }
-
-        [TestMethod]
-        public void CheckWhetherThreadsUsingThreadPoolAreNotSynchronized()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            m_ThreadsExample.StartThreadsUsingThreadPool(false);
-            Assert.IsFalse(m_ThreadsExample.IsConsistent);
-        }
-
-        [TestMethod]
-        public void CheckWhetherThreadsUsingThreadPoolAreSynchronized()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            m_ThreadsExample.StartThreadsUsingThreadPool(true);
-            Assert.IsTrue(m_ThreadsExample.IsConsistent);
-        }
-
-        [TestMethod]
-        public void MonitorMethodTest()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.MonitorMethod);
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.MonitorMethod);
-            Thread.Sleep(SleepTime); // wait for threads
-            const int _expectedCycles = 2 * 1000000; // twice the number of iterations
-            Assert.AreEqual(_expectedCycles, m_ThreadsExample.LockedNumber);
-        }
-
-        [TestMethod]
-        public void LockMethodTest()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.LockMethod);
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.LockMethod);
-            Thread.Sleep(SleepTime); // wait for threads
-            Assert.AreEqual(2 * 1000000, m_ThreadsExample.LockedNumber);
-        }
-
-        [TestMethod]
-        public void NoMonitorMethodTest()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.NoMonitorMethod);
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.NoMonitorMethod);
-            Thread.Sleep(SleepTime); // wait for threads
-            Assert.AreNotEqual(2 * 1000000, m_ThreadsExample.LockedNumber);
-        }
-
-        [TestMethod]
-        public void MonitorMethodWithTimeoutTest()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            object tab = new bool[] { false };
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.MonitorMethodWithTimeout, tab);
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.MonitorMethodWithTimeout, tab);
-            Assert.AreEqual(false, ((bool[])tab)[0]);
-            Thread.Sleep(2000); // 2 seconds - wait the timeout
-            Assert.AreEqual(true, ((bool[])tab)[0]);
-        }
-
-        [TestMethod]
-        public void WaitPulseMethodsTest()
-        {
-            CriticalSection m_ThreadsExample = new CriticalSection();
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.WaitMethod);
-            Thread.Sleep(SleepTime);
-            ThreadPool.QueueUserWorkItem(m_ThreadsExample.PulseMethod);
-            Thread.Sleep(SleepTime); // wait for threads
-            Assert.AreEqual(0, m_ThreadsExample.LockedNumber);
-        }
-
-        private const int SleepTime = 512; // ms
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      StartThreads(m_ThreadsExample.NoMonitorMethod);
+      Assert.IsFalse(m_ThreadsExample.IsConsistent);
     }
+
+    [TestMethod]
+    public void CheckWhetherThreadsAreSynchronized()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      StartThreads(m_ThreadsExample.MonitorMethod);
+      Assert.IsTrue(m_ThreadsExample.IsConsistent);
+    }
+
+    [TestMethod]
+    public void CheckWhetherThreadsUsingThreadPoolAreNotSynchronized()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      StartThreadsUsingThreadPool(m_ThreadsExample.NoMonitorMethod);
+      Assert.IsFalse(m_ThreadsExample.IsConsistent);
+    }
+
+    [TestMethod]
+    public void CheckConsitencyUsingTaskMonitor()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      StartThreadsUsingThreadPool(m_ThreadsExample.MonitorMethod);
+      Assert.IsTrue(m_ThreadsExample.IsConsistent);
+    }
+    [TestMethod]
+    public void CheckConsitencyUsingTaskNoMonitor()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      RunThreadsUsingTask(m_ThreadsExample.NoMonitorMethod);
+      Assert.IsFalse(m_ThreadsExample.IsConsistent);
+    }
+
+    [TestMethod]
+    public void CheckWhetherThreadsUsingThreadPoolAreSynchronized()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      RunThreadsUsingTask(m_ThreadsExample.MonitorMethod);
+      Assert.IsTrue(m_ThreadsExample.IsConsistent);
+    }
+
+    [TestMethod]
+    public void LockMethodTest()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      ThreadPool.QueueUserWorkItem(m_ThreadsExample.LockMethod);
+      ThreadPool.QueueUserWorkItem(m_ThreadsExample.LockMethod);
+      Thread.Sleep(SleepTime); // wait for threads
+      Assert.AreEqual(2 * 1000000, m_ThreadsExample.LockedNumber);
+    }
+
+    [TestMethod]
+    public void NoMonitorMethodTest()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      ThreadPool.QueueUserWorkItem(m_ThreadsExample.NoMonitorMethod);
+      ThreadPool.QueueUserWorkItem(m_ThreadsExample.NoMonitorMethod);
+      Thread.Sleep(SleepTime); // wait for threads
+      Assert.AreNotEqual(2 * 1000000, m_ThreadsExample.LockedNumber);
+    }
+
+    [TestMethod]
+    public void MonitorMethodWithTimeoutTest()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      object tab = new bool[] { false };
+      ThreadPool.QueueUserWorkItem(m_ThreadsExample.MonitorMethodWithTimeout, tab);
+      ThreadPool.QueueUserWorkItem(m_ThreadsExample.MonitorMethodWithTimeout, tab);
+      Assert.AreEqual(false, ((bool[])tab)[0]);
+      Thread.Sleep(2000); // 2 seconds - wait the timeout
+      Assert.AreEqual(true, ((bool[])tab)[0]);
+    }
+
+    [TestMethod]
+    public void WaitPulseMethodsTest()
+    {
+      CriticalSection m_ThreadsExample = new CriticalSection();
+      ThreadPool.QueueUserWorkItem(m_ThreadsExample.WaitMethod);
+      Thread.Sleep(SleepTime);
+      ThreadPool.QueueUserWorkItem(m_ThreadsExample.PulseMethod);
+      Thread.Sleep(SleepTime); // wait for threads
+      Assert.AreEqual(0, m_ThreadsExample.LockedNumber);
+    }
+
+    #region Test Instrumentation
+
+    private const int SleepTime = 512; // ms
+
+    public void StartThreads(ParameterizedThreadStart start)
+    {
+      Thread[] threadsArray = new Thread[2];
+      for (int i = 0; i < threadsArray.Length; i++)
+        threadsArray[i] = new Thread(start);
+      foreach (Thread _thread in threadsArray)
+        _thread.Start();
+      foreach (Thread _thread in threadsArray)
+        _thread.Join();
+    }
+
+    public void StartThreadsUsingThreadPool(WaitCallback callBack)
+    {
+      for (int i = 0; i < 2; i++)
+        ThreadPool.QueueUserWorkItem(callBack);
+      //wait for threads
+      //TODO must be improved it could cause race condition
+      Thread.Sleep(1000);
+    }
+
+    public void RunThreadsUsingTask(WaitCallback? callBack)
+    {
+      if (callBack == null)
+        throw new ArgumentNullException(nameof(callBack));
+      List<Task> _tasksInProgress = new List<Task>();
+      for (int i = 0; i < 2; i++)
+      {
+        Task newTask = Task.Run(() => callBack(null));
+        _tasksInProgress.Add(newTask);
+      }
+      //wait for threads
+      Task.WaitAll(_tasksInProgress.ToArray());
+    }
+
+    #endregion Test Instrumentation
+  }
 }
