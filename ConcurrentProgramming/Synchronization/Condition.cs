04a3883c370fe99cda6835ef478da2d6a68fef80
MPostol <mariuszpostol@gmail.com>
2024-12-18 16:36:15 +0100

    CP-Import Condition Class #433
    - done
    - reviewed comments

--- ConcurrentProgramming/Synchronization/Condition.cs
+++ ConcurrentProgramming/Synchronization/Condition.cs
@@ -15,17 +15,16 @@ namespace TP.ConcurrentProgramming.Synchronization
   /// associated with an important event (condition); hence the appearance of a signal
   /// is meant as appearance of the associated event, which manifests in establishing
   /// the relevant condition. Generally, three kinds of operations on signals are
-  /// defined, namely, wait - to suspend process until the associated signal will be
-  /// sent, Notify - to awake one of the waiting processes, and IsAwaiting - to check
-  /// if any process is waiting for the specified signal.
+  /// defined, namely, wait - to suspend thread until the associated signal will be
+  /// sent, Notify - to awake one of the waiting threads, and IsAwaiting - to check
+  /// if any thread is waiting for the specified signal.
   /// </summary>
   public sealed class Condition
   {
     private ushort _awaitingCount = 0;
 
     /// <summary>
-    /// Notifies this instance.
-    /// use Notify - to awake one of the waiting processes
+    /// Notifies a thread in the waiting queue. Use Notify to awaken one of the waiting threads if any.
     /// </summary>
     public void Notify()
     {
@@ -37,7 +36,7 @@ namespace TP.ConcurrentProgramming.Synchronization
 
     /// <summary>
     /// Notifies all.
-    /// use NotifyAll - to awake all of the waiting processes
+    /// use NotifyAll - to awake all of the waiting threads
     /// </summary>
     public void NotifyAll()
     {
@@ -84,10 +83,9 @@ namespace TP.ConcurrentProgramming.Synchronization
     }
 
     /// <summary>
-    /// Releases the lock on a monitor and blocks the current thread until
-    /// it receives Notification / Signal or a specified amount of time elapses.
+    /// Releases the lock on a monitor and blocks the current thread until it receives Notification / Signal or a specified amount of time elapses.
     /// </summary>
-    /// <param name="callingMonitor">The monitor which to releases the lock on.</param>
+    /// <param name="callingMonitor">The monitor, which to release the lock on</param>
     /// <param name="TimeOut">The number of milliseconds to wait before this method returns. </param>
     /// <returns>true if the lock was reacquired before the specified time elapsed; otherwise, false.
     /// </returns>
@@ -106,7 +104,7 @@ namespace TP.ConcurrentProgramming.Synchronization
     }
 
     /// <summary>
-    /// Determines whether this instance is awaiting. checks if any process is waiting for the specified signal
+    /// Determines whether this instance is awaiting. checks if any threads is waiting for the specified signal
     /// </summary>
     /// <returns>
     /// 	<c>true</c> if this instance is awaiting; otherwise, <c>false</c>.
