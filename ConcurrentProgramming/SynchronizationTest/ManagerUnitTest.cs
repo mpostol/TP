2c4775e368e47d52a7e6a166b69889bb550259bb
MPostol <mariuszpostol@gmail.com>
2024-12-18 18:02:49 +0100

    CP -import Manager class #434
    - fixed #434

---
+++ ConcurrentProgramming/SynchronizationTest/ManagerUnitTest.cs
@@ -0,0 +1,24 @@
+﻿
+//____________________________________________________________________________________________________________________________________
+//
+//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
+//
+//  https://github.com/mpostol/TP/discussions/182
+//
+//_____________________________________________________________________________________________________________________________________
+
+namespace TP.ConcurrentProgramming.Synchronization.Test
+{
+  [TestClass]
+  public class ManagerUnitTest
+  {
+    [TestMethod]
+    public void ConstructorTestMethod()
+    {
+      Manager monitor = new();
+      Assert.IsNotNull(monitor);
+    }
+  }
+}
