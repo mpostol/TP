91c682206cdf7f9a79effb60871e058141d14bec
MPostol <mariuszpostol@gmail.com>
2025-01-16 16:19:59 +0100

    CP - Move RunMethodAsynchronously from RealTime library #213
    - fixes #213
    - UT :+1+

--- ConcurrentProgramming/FundamentalsTest/RunMethodAsynchronouslyTests.cs
+++ ConcurrentProgramming/FundamentalsTest/RunMethodAsynchronouslyTests.cs
@@ -22,20 +22,55 @@ namespace TP.ConcurrentProgramming.FundamentalsTest
     }
 
     [TestMethod]
-    public void RunAsync_WithArgument_ShouldStartWorker()
+    public void RunAsync_WithArgument()
     {
       // Arrange
-      var doWorkHandler = new DoWorkEventHandler((sender, e) => { });
-      var completedHandler = new RunWorkerCompletedEventHandler((sender, e) => { });
+      DoWorkEventArgs? doWorkEventArgs = null;
+      int doWorkHandlerCounter = 0;
+      DoWorkEventHandler doWorkHandler = new DoWorkEventHandler((sender, e) =>
+      {
+        doWorkEventArgs = e;
+        Arguments? argument = e.Argument as Arguments;
+        if (argument != null)
+          {
+            Thread.Sleep(argument.Value); //simulate a long running operation
+            doWorkEventArgs.Result = "Done"; 
+          }
+        else
+          throw new ArgumentNullException("Argument is null");
+        doWorkHandlerCounter++;
+      });
+      RunWorkerCompletedEventArgs? runWorkerCompletedEventArgs = null;
+      int completedHandlerCounter = 0;
+      RunWorkerCompletedEventHandler completedHandler = new RunWorkerCompletedEventHandler((sender, e) =>
+      {
+        runWorkerCompletedEventArgs = e;
+        completedHandlerCounter++;
+      });
       var runner = new RunMethodAsynchronously(doWorkHandler, completedHandler);
-      var argument = new object();
+      var argument = new Arguments(100);
 
       // Act
       runner.RunAsync(argument);
+      Thread.Sleep(argument.Value * 10);
+      runner.Dispose();
 
       // Assert
-      runner.Dispose();
-      // No exception means the test passed
+
+      // Assert doWork
+      Assert.AreEqual<int>(1, doWorkHandlerCounter);
+      Assert.IsNotNull(doWorkEventArgs);
+      Assert.IsNotNull(doWorkEventArgs.Argument);
+      Assert.IsNotNull(doWorkEventArgs.Result);
+
+      // Assert completed
+      Assert.AreEqual<int>(1, completedHandlerCounter);
+      Assert.IsNotNull(runWorkerCompletedEventArgs);
+      Assert.IsNull(runWorkerCompletedEventArgs.Error);
+      Assert.IsFalse(runWorkerCompletedEventArgs.Cancelled);
+      Assert.IsNotNull(runWorkerCompletedEventArgs.Result);
+      Assert.IsInstanceOfType(runWorkerCompletedEventArgs.Result, typeof(string));
+      Assert.AreEqual<string>("Done", (string)runWorkerCompletedEventArgs.Result);
     }
 
     [TestMethod]
@@ -48,7 +83,7 @@ namespace TP.ConcurrentProgramming.FundamentalsTest
       DoWorkEventHandler doWorkHandler = new DoWorkEventHandler((sender, e) =>
       {
         doWorkSender = sender as RunMethodAsynchronously;
-        doWorkEventArgs = e;  
+        doWorkEventArgs = e;
         doWorkHandlerCounter++;
       });
       RunMethodAsynchronously? completedHandlerSender = null;
@@ -66,6 +101,7 @@ namespace TP.ConcurrentProgramming.FundamentalsTest
       // Act
       runner.RunAsync();
       Thread.Sleep(1000);
+      runner.Dispose();
 
       // Assert doWork
       Assert.AreEqual<int>(1, doWorkHandlerCounter);
@@ -80,7 +116,20 @@ namespace TP.ConcurrentProgramming.FundamentalsTest
       Assert.IsFalse(runWorkerCompletedEventArgs.Cancelled);
       Assert.IsNull(runWorkerCompletedEventArgs.Result);
       Assert.AreSame<object>(doWorkSender, completedHandlerSender);
-      runner.Dispose();
     }
+
+    #region test instrumentation
+
+    private class Arguments
+    {
+      public Arguments(int delay)
+      {
+        Value = delay;
+      }
+
+      public int Value { get; }
+    }
+
+    #endregion test instrumentation
   }
 }
